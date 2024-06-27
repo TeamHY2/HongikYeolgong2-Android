6366c4aa782625f68029efa0bec49a6a2e982b63
Bandal <no1msh1217@gmail.com>
2024-06-27 22:37:42 +0900

    chore: Ktlint 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+	alias(libs.plugins.android.application) apply false
+	alias(libs.plugins.jetbrains.kotlin.android) apply false
+	alias(libs.plugins.ktlint)
+}
+
+allprojects {
+	apply(plugin = rootProject.libs.plugins.ktlint.get().pluginId)
+
+	ktlint {
+		android = true
+	}
 }
