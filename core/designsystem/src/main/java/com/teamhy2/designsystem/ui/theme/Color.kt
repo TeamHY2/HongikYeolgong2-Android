c46094693bcefce0ee6088402e0e3bc0af2ef7e7
librarywon <librarywon@naver.com>
2024-06-29 00:46:32 +0900

    feat: design system color 추가

--- core/designsystem/src/main/java/com/teamhy2/designsystem/ui/theme/Color.kt
+++ core/designsystem/src/main/java/com/teamhy2/designsystem/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.teamhy2.designsystem.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Gray100 = Color(0xFFD3D6E0)
+val Gray200 = Color(0xFFA5A8B0)
+val Gray300 = Color(0xFF65676B)
+val Gray400 = Color(0xFF4F515A)
+val Gray600 = Color(0xFF3A3E49)
+val Gray800 = Color(0xFF23262D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+
+val Blue100 = Color(0xFF4164E2)
+val Blue200 = Color(0xFF2747BA)
+val Blue300 = Color(0xFF213065)
+val Blue400 = Color(0xFF121C42)
+
+val Yellow100 = Color(0xFFF4F33D)
+val Yellow300 = Color(0xFFCECF37)
