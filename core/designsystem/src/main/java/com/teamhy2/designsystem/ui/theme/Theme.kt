d0c116663f4891deddcb04dd3d6b970ba13783d7
librarywon <librarywon@naver.com>
2024-06-29 01:01:28 +0900

    feat: HongikYeolgong2Theme 수정

--- core/designsystem/src/main/java/com/teamhy2/designsystem/ui/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teamhy2/designsystem/ui/theme/Theme.kt
@@ -1,58 +1,61 @@
 package com.teamhy2.designsystem.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+	primary = Blue100,
+	secondary = Blue400,
+	tertiary = Yellow100,
+	background = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+	primary = Blue100,
+	secondary = Blue400,
+	tertiary = Yellow100,
+	background = Black
 )
 
 @Composable
 fun HongikYeolgong2Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	// Dynamic color is available on Android 12+
+	// dynamicColor: Boolean = true,
+	content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+	val colorScheme = when {
+		darkTheme -> DarkColorScheme
+		else -> LightColorScheme
+	}
+	val view = LocalView.current
+	if (!view.isInEditMode) {
+		SideEffect {
+			val window = (view.context as Activity).window
+			val insetsController = WindowCompat.getInsetsController(window, view)
+			window.statusBarColor = colorScheme.background.toArgb()
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+			// 항상 다크테마이기 때문에 상태바 아이콘들을 밝은 톤으로 표시
+			insetsController.isAppearanceLightStatusBars = false
+			insetsController.isAppearanceLightNavigationBars = false
+		}
+	}
+	val shapes = MaterialTheme.shapes.copy(medium = RoundedCornerShape(4.dp))
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+	MaterialTheme(
+		colorScheme = colorScheme,
+		shapes = shapes,
+		typography = Typography,
+		content = content,
+	)
 }
