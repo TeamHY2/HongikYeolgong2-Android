3b925faa9b29843f1e095defc115c4197bc01c23
librarywon <librarywon@naver.com>
2024-06-29 00:41:02 +0900

    feat: designsystem 모듈 init 파일 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	alias(libs.plugins.android.application) apply false
 	alias(libs.plugins.jetbrains.kotlin.android) apply false
 	alias(libs.plugins.ktlint)
+	alias(libs.plugins.android.library) apply false
 }
 
 allprojects {
