54a391eb3261c3f7f7c9e302d0bb18604fc222b0
Bandal <no1msh1217@gmail.com>
2024-06-29 16:09:07 +0900

    style: Ktlint CI의 정상 작동 체크를 위한 트레일링 콤마 작성

--- app/src/main/java/com/jwseo/hongikyeolgong2/MainActivity.kt
+++ app/src/main/java/com/jwseo/hongikyeolgong2/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
 				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 					Greeting(
 						name = "Android",
-						modifier = Modifier.padding(innerPadding)
+						modifier = Modifier.padding(innerPadding),
 					)
 				}
 			}
