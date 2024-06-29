7297b9bc9502ee0a3b91f7d38ec18651917c6be6
librarywon <librarywon@naver.com>
2024-06-29 12:32:58 +0900

    feat: HY2Typography가 적용된 HY2Theme 구현

--- core/designsystem/src/main/java/com/teamhy2/designsystem/ui/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teamhy2/designsystem/ui/theme/Theme.kt
@@ -2,15 +2,17 @@ package com.teamhy2.designsystem.ui.theme
 
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -27,35 +29,60 @@ private val LightColorScheme = lightColorScheme(
 	background = Black
 )
 
+private val LocalHY2Typography = staticCompositionLocalOf<HY2Typography> {
+	error("No HY2Typography provided")
+}
+
+/*
+* HY2Theme
+*
+* Typo를 변경하고 싶다면 HY2Theme.typography.title01 방식으로 접근
+* */
+object HY2Theme {
+	val typography: HY2Typography
+		@Composable
+		@ReadOnlyComposable
+		get() = LocalHY2Typography.current
+}
+
 @Composable
-fun HongikYeolgong2Theme(
-	darkTheme: Boolean = isSystemInDarkTheme(),
-	// Dynamic color is available on Android 12+
-	// dynamicColor: Boolean = true,
+fun ProvideHY2Typography(
+	typography: HY2Typography,
 	content: @Composable () -> Unit
 ) {
-	val colorScheme = when {
-		darkTheme -> DarkColorScheme
-		else -> LightColorScheme
-	}
+	val provideTypography = remember { typography.copy() }.apply { update(typography) }
+	CompositionLocalProvider(
+		LocalHY2Typography provides provideTypography,
+		content = content
+	)
+}
+
+@Composable
+fun HY2Theme(
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	content: @Composable () -> Unit
+) {
+	val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 	val view = LocalView.current
 	if (!view.isInEditMode) {
 		SideEffect {
 			val window = (view.context as Activity).window
 			val insetsController = WindowCompat.getInsetsController(window, view)
-			window.statusBarColor = colorScheme.background.toArgb()
+			val statusBarColor = colorScheme.background.toArgb()
+			window.statusBarColor = statusBarColor
 
 			// 항상 다크테마이기 때문에 상태바 아이콘들을 밝은 톤으로 표시
 			insetsController.isAppearanceLightStatusBars = false
-			insetsController.isAppearanceLightNavigationBars = false
 		}
 	}
-	val shapes = MaterialTheme.shapes.copy(medium = RoundedCornerShape(4.dp))
 
-	MaterialTheme(
-		colorScheme = colorScheme,
-		shapes = shapes,
-		typography = Typography,
-		content = content,
-	)
+	val typography = HY2Typography()
+
+	ProvideHY2Typography(typography) {
+		MaterialTheme(
+			colorScheme = colorScheme,
+			content = content
+		)
+	}
 }
+
