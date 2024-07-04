f01cf5fd1a8a843edfdadd893dc33d8849ad5e7c
Bandal <no1msh1217@gmail.com>
2024-07-04 23:43:34 +0900

    chore: Compose를 사용하는 Android 모듈을 위한 빌드로직 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	alias(libs.plugins.jetbrains.kotlin.android) apply false
 	alias(libs.plugins.ktlint)
 	alias(libs.plugins.android.library) apply false
+	alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
 
 allprojects {
