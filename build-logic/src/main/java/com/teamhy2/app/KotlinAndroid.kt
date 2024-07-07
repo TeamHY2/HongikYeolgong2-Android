2f2a6da1842f837b249e157ede3475241fd321b4
Bandal <no1msh1217@gmail.com>
2024-07-07 21:37:53 +0900

    chore: Kotlin과 Android를 사용하는 모듈을 위한 빌드로직 구현

---
+++ build-logic/src/main/java/com/teamhy2/app/KotlinAndroid.kt
@@ -0,0 +1,49 @@
+package com.teamhy2.app
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun Project.configureKotlinAndroid() {
+	// Plugins
+	pluginManager.apply("org.jetbrains.kotlin.android")
+
+	// Android
+	androidExtension.apply {
+		compileSdk = 34
+		defaultConfig {
+			minSdk = 30
+			testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		}
+
+		buildTypes {
+			getByName("release") {
+				isMinifyEnabled = false
+				proguardFiles(
+					getDefaultProguardFile("proguard-android-optimize.txt"),
+					"proguard-rules.pro",
+				)
+			}
+		}
+		compileOptions {
+			sourceCompatibility = JavaVersion.VERSION_17
+			targetCompatibility = JavaVersion.VERSION_17
+		}
+		packaging {
+			resources {
+				excludes += "/META-INF/{AL2.0,LGPL2.1}"
+			}
+		}
+	}
+
+	configureKotlin()
+}
+
+fun Project.configureKotlin() {
+	tasks.withType<KotlinCompile>().configureEach {
+		kotlinOptions {
+			jvmTarget = JavaVersion.VERSION_17.toString()
+		}
+	}
+}
