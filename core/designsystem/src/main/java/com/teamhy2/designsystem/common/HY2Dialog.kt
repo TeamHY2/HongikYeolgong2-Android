cfe43bccd1f1d84b9cc575800ed17269a468cbce
librarywon <librarywon@naver.com>
2024-07-18 17:10:06 +0900

    feat: Dim 처리율 변경

--- core/designsystem/src/main/java/com/teamhy2/designsystem/common/HY2Dialog.kt
+++ core/designsystem/src/main/java/com/teamhy2/designsystem/common/HY2Dialog.kt
@@ -20,10 +20,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
 import com.teamhy2.designsystem.ui.theme.Blue100
 import com.teamhy2.designsystem.ui.theme.Gray100
 import com.teamhy2.designsystem.ui.theme.Gray200
@@ -57,6 +59,7 @@ fun HY2Dialog(
         onDismissRequest = onDismiss,
         properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = false),
     ) {
+        (LocalView.current.parent as DialogWindowProvider).window.setDimAmount(0.75f)
         Surface(
             modifier =
                 modifier
