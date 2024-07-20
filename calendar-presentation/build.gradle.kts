e6dec42ca6a797fc0660ced0066bd6e1d628b03e
Bandal <no1msh1217@gmail.com>
2024-07-20 23:31:52 +0900

    chore: 캘린더 프레젠테이션 모듈 생성

---
+++ calendar-presentation/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("hongikyeolgong2.android.feature")
+}
+
+android {
+    namespace = "com.teamhy2.hongikyeolgong2.calendar.presentation"
+}
---
+++ calendar-presentation/src/main/java/com/hongikyeolgong2/calendar/presentation/Calendar.kt
@@ -0,0 +1,13 @@
+package com.hongikyeolgong2.calendar.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Hy2Calendar(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "test")
+    }
+}
