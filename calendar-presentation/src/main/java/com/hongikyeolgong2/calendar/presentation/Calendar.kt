bdd73963defff21e32a68177e0eda86bf51138ff
Bandal <no1msh1217@gmail.com>
2024-07-22 23:54:52 +0900

    feat: 캘린더 일 표시 구현

--- calendar-presentation/src/main/java/com/hongikyeolgong2/calendar/presentation/Calendar.kt
+++ calendar-presentation/src/main/java/com/hongikyeolgong2/calendar/presentation/Calendar.kt
@@ -1,36 +1,54 @@
 package com.hongikyeolgong2.calendar.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hongikyeolgong2.calendar.model.Calendar
+import com.hongikyeolgong2.calendar.model.StudyDay
 import com.teamhy2.designsystem.R
 import com.teamhy2.designsystem.ui.theme.Gray100
 import com.teamhy2.designsystem.ui.theme.Gray300
+import com.teamhy2.designsystem.ui.theme.Gray800
 import com.teamhy2.designsystem.ui.theme.HY2Theme
 import com.teamhy2.designsystem.ui.theme.HY2Typography
+import com.teamhy2.designsystem.ui.theme.White
 import com.teamhy2.hongikyeolgong2.calendar.presentation.R.string.description_next_month
 import com.teamhy2.hongikyeolgong2.calendar.presentation.R.string.description_previous_month
 
 @Composable
-fun Hy2Calendar(modifier: Modifier = Modifier) {
+fun Hy2Calendar(
+    calendar: Calendar,
+    onPreviousMonthClick: () -> Unit,
+    onNextMonthClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Column(modifier = modifier) {
         CalendarHeader(
-            title = "Jan 2024",
-            onPreviousMonthClick = { /*TODO*/ },
-            onNextMonthClick = { /*TODO*/ },
+            title = calendar.now,
+            onPreviousMonthClick = onPreviousMonthClick,
+            onNextMonthClick = onNextMonthClick,
         )
         Row(modifier = Modifier.padding(bottom = 8.dp)) {
             DayOfWeek.entries.forEach { dayOfWeek ->
@@ -38,6 +56,15 @@ fun Hy2Calendar(modifier: Modifier = Modifier) {
                     name = dayOfWeek.abbreviation,
                     modifier = Modifier.weight(1f),
                 )
+            }
+        }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(7),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            items(calendar.getMonth()) {
+                Day(studyDay = it, modifier = Modifier.weight(1f))
             }
         }
     }
@@ -99,11 +126,36 @@ fun DayOfWeek(
     )
 }
 
+@Composable
+fun Day(
+    studyDay: StudyDay,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier =
+            modifier
+                .height(33.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(Gray800),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = studyDay.date.dayOfMonth.toString(),
+            style = HY2Typography().body03,
+            color = White,
+        )
+    }
+}
+
 @Preview
 @Composable
 private fun Hy2CalendarPreview() {
     HY2Theme {
-        Hy2Calendar()
+        Hy2Calendar(
+            calendar = Calendar(),
+            onPreviousMonthClick = {},
+            onNextMonthClick = {},
+        )
     }
 }
 
