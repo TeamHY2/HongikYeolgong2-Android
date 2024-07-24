b9f976adcf5faffee898ee5eb8ce35d32125e4a3
Bandal <no1msh1217@gmail.com>
2024-07-24 21:29:29 +0900

    chore: 임시로 메인액티비티에 넣은 컴포저블 제거

--- main-presentation/src/main/java/com/teamhy2/feature/main/MainActivity.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/MainActivity.kt
@@ -12,9 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.hongikyeolgong2.calendar.model.Calendar
-import com.hongikyeolgong2.calendar.presentation.Hy2Calendar
 import com.teamhy2.designsystem.ui.theme.HY2Theme
 import com.teamhy2.designsystem.ui.theme.White
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,12 +26,7 @@ class MainActivity : AppCompatActivity() {
             HY2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
-                        Hy2Calendar(
-                            calendar = Calendar(),
-                            onPreviousMonthClick = { /* TODO: 구현 필요 */ },
-                            onNextMonthClick = { /* TODO: 구현 필요 */ },
-                            modifier = Modifier.padding(horizontal = 26.dp),
-                        )
+                        Text("Hello World!")
                     }
                 }
             }
