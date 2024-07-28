0dc1909787d4aa8241b5322d086587ba03248f56
librarywon <librarywon@naver.com>
2024-07-28 23:44:06 +0900

    feat: timer-presentation gradle 설정

--- timer-presentation/build.gradle.kts
+++ timer-presentation/build.gradle.kts
@@ -1,43 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("hongikyeolgong2.android.feature")
 }
 
 android {
-    namespace = "com.teamhy2.hongikyeolgong2.timer"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 30
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    namespace = "com.teamhy2.hongikyeolgong2.timer.presentation"
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(projects.timerDomain)
 }
