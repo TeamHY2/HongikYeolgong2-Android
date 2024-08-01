f84d7c080f52d37469e18a3f1098dcef6fc11219
librarywon <librarywon@naver.com>
2024-08-01 13:27:23 +0900

    feat: Repository bind 모듈 구현

--- setting-data/src/main/java/com/teamhy2/feature/setting/data/di/SettingModule.kt
+++ setting-data/src/main/java/com/teamhy2/feature/setting/data/di/SettingModule.kt
@@ -1,20 +1,33 @@
 package com.teamhy2.feature.setting.data.di
 
 import android.content.Context
-import com.teamhy2.feature.setting.data.datasource.SettingsDataSource
+import com.teamhy2.feature.setting.data.datasource.LocalSettingsDataSource
+import com.teamhy2.feature.setting.data.repository.SettingsRepositoryImpl
+import com.teamhy2.feature.setting.domain.repository.SettingsRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object SettingModule {
+object SettingsDataSourceModule {
     @Provides
-    fun provideSettingsDataSource(
+    @Singleton
+    fun provideLocalSettingsDataSource(
         @ApplicationContext context: Context,
-    ): SettingsDataSource {
-        return SettingsDataSource(context)
+    ): LocalSettingsDataSource {
+        return LocalSettingsDataSource(context)
     }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class SettingsModule {
+    @Binds
+    @Singleton
+    abstract fun bindSettingsRepository(settingsRepositoryImpl: SettingsRepositoryImpl): SettingsRepository
 }
