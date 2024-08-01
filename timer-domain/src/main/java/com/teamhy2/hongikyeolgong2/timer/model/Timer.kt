f6b1db2143e1160ca8b392faf08a526713e18d75
librarywon <librarywon@naver.com>
2024-08-01 14:34:23 +0900

    refactor: init 블럭으로 require() 이전

--- timer-domain/src/main/java/com/teamhy2/hongikyeolgong2/timer/model/Timer.kt
+++ timer-domain/src/main/java/com/teamhy2/hongikyeolgong2/timer/model/Timer.kt
@@ -6,13 +6,19 @@ import kotlinx.coroutines.flow.flow
 import java.time.Duration
 import java.time.LocalTime
 
-class Timer private constructor(
+class Timer(
     private val startTime: LocalTime,
     private val duration: Duration,
     private val events: Map<Long, () -> Unit>,
 ) {
     private var endTime: LocalTime = startTime.plusSeconds(duration.seconds)
     private var leftTime: Duration = duration
+
+    init {
+        require(events.keys.containsAll(EVENT_TIMES)) {
+            "포함되지 않은 시간이 있습니다."
+        }
+    }
 
     val formattedLeftTime: String
         get() =
@@ -36,9 +42,7 @@ class Timer private constructor(
         flow {
             while (!isTimeOver()) {
                 tick()
-                if (events.containsKey(leftSeconds)) {
-                    events[leftSeconds]?.invoke()
-                }
+                events[leftSeconds]?.invoke()
                 emit(leftSeconds)
                 delay(1000)
             }
@@ -61,18 +65,6 @@ class Timer private constructor(
         const val FIVE_MINUTES_SECONDS: Long = 5 * 60L
         const val TIME_OVER_SECONDS: Long = 0L
 
-        private val eventTimes: List<Long> =
-            listOf(THIRTY_MINUTES_SECONDS, FIVE_MINUTES_SECONDS, TIME_OVER_SECONDS)
-
-        fun create(
-            startTime: LocalTime,
-            duration: Duration,
-            events: Map<Long, () -> Unit>,
-        ): Timer {
-            require(events.keys.containsAll(eventTimes)) {
-                "포함되지 않은 시간이 있습니다."
-            }
-            return Timer(startTime, duration, events)
-        }
+        private val EVENT_TIMES: List<Long> = listOf(THIRTY_MINUTES_SECONDS, FIVE_MINUTES_SECONDS, TIME_OVER_SECONDS)
     }
 }
