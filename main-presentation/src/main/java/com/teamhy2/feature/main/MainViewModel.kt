378b629a737223a595e739e8d75cab6e2223d4c7
librarywon <librarywon@naver.com>
2024-08-19 18:47:00 +0900

    refactor: MainViewModel @Inject 추가

--- main-presentation/src/main/java/com/teamhy2/feature/main/MainViewModel.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/MainViewModel.kt
@@ -2,6 +2,9 @@ package com.teamhy2.feature.main
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel()
+class MainViewModel
+    @Inject
+    constructor() : ViewModel()
