758c8c119d6408fa94cbe39acc4d101f4f55c358
librarywon <librarywon@naver.com>
2024-08-20 04:13:53 +0900

    feat: NavHost Webview 이동 로직 추가

--- main-presentation/src/main/java/com/teamhy2/feature/main/HY2NavHost.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/HY2NavHost.kt
@@ -10,6 +10,12 @@ import androidx.navigation.compose.NavHost
 import com.teamhy2.designsystem.ui.theme.Black
 import com.teamhy2.feature.main.navigation.mainScreen
 import com.teamhy2.feature.main.navigation.navigateToMain
+import com.teamhy2.feature.main.webviews.inquiry.navigation.inquiryScreen
+import com.teamhy2.feature.main.webviews.inquiry.navigation.navigateToInquiry
+import com.teamhy2.feature.main.webviews.notice.navigation.navigateToNotice
+import com.teamhy2.feature.main.webviews.notice.navigation.noticeScreen
+import com.teamhy2.feature.main.webviews.seatingChart.navigation.navigateToSeatingChart
+import com.teamhy2.feature.main.webviews.seatingChart.navigation.seatingChartScreen
 import com.teamhy2.feature.setting.presentation.navigation.navigateToSetting
 import com.teamhy2.feature.setting.presentation.navigation.settingScreen
 import com.teamhy2.onboarding.navigation.Onboarding
@@ -18,6 +24,7 @@ import com.teamhy2.onboarding.navigation.onboardingScreen
 @Composable
 fun HY2NavHost(
     navController: NavHostController,
+    urls: Map<String, String>,
     modifier: Modifier = Modifier,
     startDestination: String = Onboarding.ROUTE,
 ) {
@@ -40,14 +47,28 @@ fun HY2NavHost(
             // TODO onSeatingChartClick 스크린 연결 필요
             mainScreen(
                 onSettingClick = navController::navigateToSetting,
-                onSeatingChartClick = navController::navigateToSetting,
+                onSeatingChartClick = navController::navigateToSeatingChart,
             )
 
-            // TODO onNoticeClick, onInquiryClick 스크린 연결 필요
+            seatingChartScreen(
+                url = urls["seatingChart"] ?: "",
+                onCloseButtonClick = navController::popBackStack,
+            )
+
+            noticeScreen(
+                url = urls["notice"] ?: "",
+                onCloseButtonClick = navController::popBackStack,
+            )
+
+            inquiryScreen(
+                url = urls["inquiry"] ?: "",
+                onCloseButtonClick = navController::popBackStack,
+            )
+
             settingScreen(
                 onBackButtonClick = navController::popBackStack,
-                onNoticeClick = navController::popBackStack,
-                onInquiryClick = navController::popBackStack,
+                onNoticeClick = navController::navigateToNotice,
+                onInquiryClick = navController::navigateToInquiry,
             )
         }
     }
