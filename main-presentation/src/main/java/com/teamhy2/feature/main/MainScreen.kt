48b49befd5126b9b9d250a0ef88e784a2e282a2d
librarywon <librarywon@naver.com>
2024-08-19 16:52:58 +0900

    feat: MainViewModel 생성 및 반영

--- main-presentation/src/main/java/com/teamhy2/feature/main/MainScreen.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/MainScreen.kt
@@ -6,17 +6,20 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun MainRoute(
     onSettingClick: () -> Unit,
     onSeatingChartClick: () -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: MainViewModel = hiltViewModel(),
 ) {
     MainScreen(
         onSettingClick = onSettingClick,
         onSeatingChartClick = onSeatingChartClick,
         modifier = modifier,
+        viewModel = viewModel,
     )
 }
 
@@ -25,6 +28,7 @@ fun MainScreen(
     onSettingClick: () -> Unit,
     onSeatingChartClick: () -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: MainViewModel,
 ) {
     Column(modifier = modifier.fillMaxSize()) {
         Button(onClick = onSettingClick) {
