c8cbb07da3f73e168522677e1789dd7e2c444586
librarywon <librarywon@naver.com>
2024-08-19 19:30:48 +0900

    refactor: Preview를 대응하기 위한 Screen 생성자 수정

--- main-presentation/src/main/java/com/teamhy2/feature/main/MainScreen.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/MainScreen.kt
@@ -5,8 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.teamhy2.designsystem.ui.theme.HY2Theme
+import com.teamhy2.feature.main.model.MainUiState
 
 @Composable
 fun MainRoute(
@@ -15,11 +22,13 @@ fun MainRoute(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel = hiltViewModel(),
 ) {
+    val uiState: MainUiState = viewModel.mainUiState.collectAsState().value
+
     MainScreen(
         onSettingClick = onSettingClick,
         onSeatingChartClick = onSeatingChartClick,
         modifier = modifier,
-        viewModel = viewModel,
+        uiState = uiState,
     )
 }
 
@@ -28,11 +37,25 @@ fun MainScreen(
     onSettingClick: () -> Unit,
     onSeatingChartClick: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: MainViewModel,
+    uiState: MainUiState,
 ) {
     Column(modifier = modifier.fillMaxSize()) {
         Button(onClick = onSettingClick) {
             Text(text = "Setting")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainScreenPreview() {
+    val state by remember { mutableStateOf(MainUiState()) }
+
+    HY2Theme {
+        MainScreen(
+            onSettingClick = { },
+            onSeatingChartClick = { },
+            uiState = state,
+        )
     }
 }
--- main-presentation/src/main/java/com/teamhy2/feature/main/MainViewModel.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/MainViewModel.kt
@@ -1,10 +1,16 @@
 package com.teamhy2.feature.main
 
 import androidx.lifecycle.ViewModel
+import com.teamhy2.feature.main.model.MainUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel
     @Inject
-    constructor() : ViewModel()
+    constructor() : ViewModel() {
+        private val _mainUiState = MutableStateFlow(MainUiState())
+        val mainUiState: StateFlow<MainUiState> = _mainUiState
+    }
