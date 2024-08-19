1d86c8ef1f71628c9db8742a161177b1fbac4048
librarywon <librarywon@naver.com>
2024-08-20 04:06:12 +0900

    feat: main-data gradle 세팅

--- main-data/build.gradle.kts
+++ main-data/build.gradle.kts
@@ -1,43 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("hongikyeolgong2.android.feature")
 }
 
 android {
-    namespace = "com.teamhy2.main.data"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 30
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    namespace = "com.teamhy2.hongikyeolgong2.main.data"
 }
 
 dependencies {
+    implementation(projects.mainDomain)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.firebase.firestore.ktx)
 }
