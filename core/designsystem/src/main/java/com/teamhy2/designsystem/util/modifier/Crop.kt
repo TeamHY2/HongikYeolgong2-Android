a0962598242b8c469f0b3bb17bcf6369ad254a8d
Bandal <no1msh1217@gmail.com>
2024-08-22 00:43:21 +0900

    feat: 컴포넌트를 자를 수 있는 modifier 함수 구현

---
+++ core/designsystem/src/main/java/com/teamhy2/designsystem/util/modifier/Crop.kt
@@ -0,0 +1,23 @@
+package com.teamhy2.designsystem.util.modifier
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+fun Modifier.crop(
+    horizontal: Dp = 0.dp,
+    vertical: Dp = 0.dp,
+): Modifier =
+    this.layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        fun Dp.toPxInt(): Int = this.toPx().toInt()
+
+        layout(
+            placeable.width - (horizontal * 2).toPxInt(),
+            placeable.height - (vertical * 2).toPxInt(),
+        ) {
+            placeable.placeRelative(-horizontal.toPx().toInt(), -vertical.toPx().toInt())
+        }
+    }
