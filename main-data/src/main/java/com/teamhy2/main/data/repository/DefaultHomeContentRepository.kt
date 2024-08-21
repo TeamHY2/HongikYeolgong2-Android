b0a456ff279eefb0eaaa1aba872ea24c8428c011
librarywon <librarywon@naver.com>
2024-08-21 18:11:14 +0900

    feat: HomeContentRepository, 구현체 구현

---
+++ main-data/src/main/java/com/teamhy2/main/data/repository/DefaultHomeContentRepository.kt
@@ -0,0 +1,33 @@
+package com.teamhy2.main.data.repository
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.teamhy2.main.domain.HomeContentRepository
+import com.teamhy2.main.model.WiseSaying
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+import kotlin.random.Random
+
+class DefaultHomeContentRepository
+    @Inject
+    constructor() : HomeContentRepository {
+        private val firestore = FirebaseFirestore.getInstance()
+
+        override suspend fun fetchWiseSaying(): WiseSaying {
+            val result = firestore.collection(WISE_SAYINGS_COLLECTION).get().await()
+            if (result.isEmpty) return WiseSaying("", "")
+
+            val randomIndex = Random.nextInt(result.size())
+            val document = result.documents[randomIndex]
+
+            val quote = document.getString(WISE_SAYINGS_QUOTE_FIELD) ?: ""
+            val author = document.getString(WISE_SAYINGS_AUTHOR_FIELD) ?: ""
+
+            return WiseSaying(quote, author)
+        }
+
+        companion object {
+            private const val WISE_SAYINGS_COLLECTION = "WiseSaying"
+            private const val WISE_SAYINGS_QUOTE_FIELD = "quote"
+            private const val WISE_SAYINGS_AUTHOR_FIELD = "author"
+        }
+    }
