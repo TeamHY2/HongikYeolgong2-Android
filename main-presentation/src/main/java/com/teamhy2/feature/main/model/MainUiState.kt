6ca63935aeff5c6f0398cced59e5f12542c94e22
librarywon <librarywon@naver.com>
2024-08-21 18:16:03 +0900

    feat: MainUiState에 wiseSaying 속성 추가

--- main-presentation/src/main/java/com/teamhy2/feature/main/model/MainUiState.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/model/MainUiState.kt
@@ -1,5 +1,8 @@
 package com.teamhy2.feature.main.model
 
+import com.teamhy2.main.model.WiseSaying
+
 data class MainUiState(
     val isTimerRunning: Boolean = false,
+    val wiseSaying: WiseSaying = WiseSaying("", ""),
 )
