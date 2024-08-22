1f219fb0e9cbd85222e81aa32da86c69e3b92185
librarywon <librarywon@naver.com>
2024-08-22 18:23:41 +0900

    feat: MainImageButton 리플 효과 제거

--- main-presentation/src/main/java/com/teamhy2/feature/main/component/MainImageButton.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/component/MainImageButton.kt
@@ -1,15 +1,22 @@
 package com.teamhy2.feature.main.component
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.teamhy2.designsystem.ui.theme.HY2Typography
 import com.teamhy2.hongikyeolgong2.main.presentation.R
 
@@ -20,18 +27,20 @@ fun MainImageButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Button(
-        colors =
-            ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent,
-            ),
-        onClick = onClick,
+    Box(
         modifier =
             modifier
+                .fillMaxSize()
                 .paint(
                     painter = painterResource(id = imageResId),
                     contentScale = ContentScale.FillBounds,
+                )
+                .clickable(
+                    onClick = onClick,
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
                 ),
+        contentAlignment = Alignment.Center,
     ) {
         Text(
             text = text,
@@ -48,5 +57,9 @@ fun MainImageButtonPreview() {
         imageResId = R.drawable.img_seating_chart_button_background,
         text = "좌석",
         onClick = { },
+        modifier =
+            Modifier
+                .height(72.dp)
+                .width(150.dp),
     )
 }
