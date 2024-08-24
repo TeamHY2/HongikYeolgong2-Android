5fc922759fbbb6984713c85876153b1582b400d3
librarywon <librarywon@naver.com>
2024-08-24 15:37:23 +0900

    feat: MainUiState 생성자 변수 추가

--- main-presentation/src/main/java/com/teamhy2/feature/main/model/MainUiState.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/model/MainUiState.kt
@@ -1,8 +1,15 @@
 package com.teamhy2.feature.main.model
 
+import com.hongikyeolgong2.calendar.model.Calendar
+import com.teamhy2.hongikyeolgong2.timer.prsentation.model.TimerUiModel
 import com.teamhy2.main.model.WiseSaying
+import java.time.LocalTime
 
 data class MainUiState(
     val isTimerRunning: Boolean = false,
+    val isTimePickerVisible: Boolean = false,
     val wiseSaying: WiseSaying = WiseSaying("", ""),
+    val selectedTime: LocalTime = LocalTime.now(),
+    val calendar: Calendar = Calendar(studyDays = emptyList()),
+    val timerState: TimerUiModel = TimerUiModel(),
 )
