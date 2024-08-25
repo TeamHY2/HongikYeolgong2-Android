8832aeaca0249e72689e44f5a405b202226327b9
Bandal <no1msh1217@gmail.com>
2024-08-25 17:47:19 +0900

    feat: 유저 레포지토리에서 등록된 유저인지 확인할 수 있다

--- onboarding-data/src/main/java/com/teamhy2/onboarding/data/repository/DefaultUserRepository.kt
+++ onboarding-data/src/main/java/com/teamhy2/onboarding/data/repository/DefaultUserRepository.kt
@@ -3,6 +3,7 @@ package com.teamhy2.onboarding.data.repository
 import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.teamhy2.onboarding.domain.repository.UserRepository
@@ -10,39 +11,51 @@ import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class DefaultUserRepository
-    @Inject
-    constructor() : UserRepository {
-        private val firestore = FirebaseFirestore.getInstance()
-
-        override suspend fun checkNicknameDuplication(nickname: String): Boolean {
-            try {
-                val result: QuerySnapshot = firestore.collection(FIREBASE_USER_COLLECTION).get().await()
-                result.documents.find { it.getString(FIREBASE_NICKNAME_DOCUMENT_ID) == nickname }
-                    ?.let { return true }
-            } catch (e: Exception) {
-                Log.d("DefaultRepository", "Error ", e)
-            }
-
-            return false
-        }
-
-        override suspend fun signUp(
-            nickname: String,
-            department: String,
-        ) {
-            val user =
-                hashMapOf(
-                    FIREBASE_NICKNAME_DOCUMENT_ID to nickname,
-                    "department" to department,
-                )
-
-            firestore.collection(FIREBASE_USER_COLLECTION)
-                .document(Firebase.auth.currentUser?.uid!!)
-                .set(user)
-        }
-
-        companion object {
-            private const val FIREBASE_USER_COLLECTION = "User"
-            private const val FIREBASE_NICKNAME_DOCUMENT_ID = "nickname"
-        }
+@Inject
+constructor() : UserRepository {
+    private val firestore = FirebaseFirestore.getInstance()
+    override suspend fun checkUserExists(uid:String): Boolean {
+        val result: QuerySnapshot = firestore.collection(FIREBASE_USER_COLLECTION).get().await()
+        val user: DocumentSnapshot? =
+            result.documents.find { it.id == uid }
+        user?.let { return true }
+        return false
     }
+
+    override suspend fun checkNicknameDuplication(nickname: String): Boolean {
+        try {
+            val result: QuerySnapshot = firestore.collection(FIREBASE_USER_COLLECTION).get().await()
+            result.documents.find { it.getString(NICKNAME_KEY) == nickname }
+                ?.let { return true }
+        } catch (e: Exception) {
+            Log.d("checkNicknameDuplication", "Error ", e)
+        }
+
+        return false
+    }
+
+    override suspend fun signUp(
+        nickname: String,
+        department: String,
+    ) {
+        val user =
+            hashMapOf(
+                NICKNAME_KEY to nickname,
+                DEPARTMENT_KEY to department,
+                EMAIL_KEY to Firebase.auth.currentUser?.email,
+                ID_KEY to Firebase.auth.currentUser?.uid,
+            )
+
+        firestore.collection(FIREBASE_USER_COLLECTION)
+            .document(Firebase.auth.currentUser?.uid!!)
+            .set(user)
+    }
+
+    companion object {
+        private const val FIREBASE_USER_COLLECTION = "User"
+        private const val NICKNAME_KEY = "nickname"
+        private const val DEPARTMENT_KEY = "department"
+        private const val EMAIL_KEY = "email"
+        private const val ID_KEY = "id"
+    }
+}
