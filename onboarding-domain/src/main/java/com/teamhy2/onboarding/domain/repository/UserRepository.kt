e578824ffe81bd423bb3c6d6ca150a88640a0072
Bandal <no1msh1217@gmail.com>
2024-08-25 17:47:19 +0900

    feat: 유저 레포지토리에서 등록된 유저인지 확인할 수 있다

--- onboarding-domain/src/main/java/com/teamhy2/onboarding/domain/repository/UserRepository.kt
+++ onboarding-domain/src/main/java/com/teamhy2/onboarding/domain/repository/UserRepository.kt
@@ -1,6 +1,8 @@
 package com.teamhy2.onboarding.domain.repository
 
 interface UserRepository {
+    suspend fun checkUserExists(uid: String): Boolean
+
     suspend fun checkNicknameDuplication(nickname: String): Boolean
 
     suspend fun signUp(
