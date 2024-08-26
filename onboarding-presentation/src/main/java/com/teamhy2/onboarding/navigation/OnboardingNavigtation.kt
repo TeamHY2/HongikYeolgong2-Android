f9e8e6b51fea8eb86de016e83b697c46101fa133
Bandal <no1msh1217@gmail.com>
2024-08-26 15:17:56 +0900

    refactor: popUpToOnboarding으로 이름 변경

--- onboarding-presentation/src/main/java/com/teamhy2/onboarding/navigation/OnboardingNavigtation.kt
+++ onboarding-presentation/src/main/java/com/teamhy2/onboarding/navigation/OnboardingNavigtation.kt
@@ -6,7 +6,7 @@ import androidx.navigation.compose.composable
 import com.teamhy2.onboarding.OnboardingRoute
 import com.teamhy2.onboarding.navigation.Onboarding.ROUTE
 
-fun NavController.navigateToOnboarding() {
+fun NavController.popUpToOnboarding() {
     // back stack screen을 모두 제거하고 이동 합니다.
     navigate(ROUTE) {
         popUpTo(graph.startDestinationId) { inclusive = true }
