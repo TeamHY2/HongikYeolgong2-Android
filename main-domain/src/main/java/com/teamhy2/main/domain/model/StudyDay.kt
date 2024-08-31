4658498062ec1a13b0380af4b2f9010545cbd64e
librarywon <librarywon@naver.com>
2024-08-31 15:07:37 +0900

    refactor: 문자열 상수화, 초단위 계산 로직으로 변경

--- main-domain/src/main/java/com/teamhy2/main/domain/model/StudyDay.kt
+++ main-domain/src/main/java/com/teamhy2/main/domain/model/StudyDay.kt
@@ -5,11 +5,16 @@ import java.time.LocalDate
 data class StudyDay(
     val date: LocalDate,
     val secondDuration: Long,
-)
+) {
+    companion object {
+        const val DATE_KEY = "date"
+        const val SECOND_DURATION_KEY = "secondDuration"
+    }
+}
 
 fun StudyDay.toMap(): Map<String, Any> {
     return mapOf(
-        "date" to this.date.toString(),
-        "secondDuration" to this.secondDuration,
+        StudyDay.DATE_KEY to this.date.toString(),
+        StudyDay.SECOND_DURATION_KEY to this.secondDuration,
     )
 }
