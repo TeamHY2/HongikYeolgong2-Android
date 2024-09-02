37fc10cfa0dba5fe3c7ee81dd0f43959435eb625
librarywon <librarywon@naver.com>
2024-09-03 02:20:36 +0900

    feat: addStudyDay, getStudyDaysByMonth 로직 구현

--- main-domain/src/main/java/com/teamhy2/main/domain/repository/StudyDayRepository.kt
+++ main-domain/src/main/java/com/teamhy2/main/domain/repository/StudyDayRepository.kt
@@ -1,8 +1,12 @@
 package com.teamhy2.main.domain.repository
 
+import com.teamhy2.main.domain.model.StudyDayResponse
+
 interface StudyDayRepository {
     suspend fun addStudyDay(
         uid: String,
         startTime: String,
     )
+
+    suspend fun getStudyDaysByMonth(uid: String): Map<String, List<StudyDayResponse>>
 }
