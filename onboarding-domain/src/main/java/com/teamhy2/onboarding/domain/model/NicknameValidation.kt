dac874ea8e00d80ce5fe4576e34f4a0c65471e08
Bandal <no1msh1217@gmail.com>
2024-09-10 23:14:48 +0900

    fix: 닉네임 규칙에 숫자까지 포함 시킴

--- onboarding-domain/src/main/java/com/teamhy2/onboarding/domain/model/NicknameValidation.kt
+++ onboarding-domain/src/main/java/com/teamhy2/onboarding/domain/model/NicknameValidation.kt
@@ -2,7 +2,7 @@ package com.teamhy2.onboarding.domain.model
 
 object NicknameValidation {
     fun validate(nickname: String): Boolean {
-        val regex = "^[가-힣a-zA-Z]{2,8}$".toRegex()
+        val regex = "^[가-힣a-zA-Z0-9]{2,8}$".toRegex()
         return regex.matches(nickname)
     }
 }
