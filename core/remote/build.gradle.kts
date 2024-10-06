139c6cc3a0772f303c0183a22964f854cd9ca9e6
Bandal <no1msh1217@gmail.com>
2024-10-06 13:06:04 +0900

    feat: 네트워크 모듈 구현

---
+++ core/remote/build.gradle.kts
@@ -0,0 +1,30 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    id("hongikyeolgong2.android.library")
+}
+
+android {
+    namespace = "com.benenfeldt.remote"
+
+    defaultConfig {
+        buildConfigField("String", "BASE_URL", getApiKey("BASE_URL"))
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.retrofit.adapters.result)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+
+}
+
+fun getApiKey(propertyKey: String): String {
+    return gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
+}
