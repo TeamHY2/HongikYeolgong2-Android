729b2166f219b65cdd5b99d1584533b4d916b07d
Bandal <no1msh1217@gmail.com>
2024-10-13 21:22:30 +0900

    feat: 유저 로그인 및 회원 가입 서버 통신 구축

---
+++ core/remote/src/main/java/com/benenfeldt/remote/api/UserService.kt
@@ -0,0 +1,26 @@
+package com.benenfeldt.remote.api
+
+import com.benenfeldt.remote.dto.UserSignInRequest
+import com.benenfeldt.remote.dto.UserSignInResponse
+import com.benenfeldt.remote.dto.UserSignUpRequest
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface UserService {
+    @GET("/api/v1/user/duplicate-nickname")
+    suspend fun checkNicknameDuplication(
+        @Query("nickname") nickname: String,
+    ): Result<Boolean>
+
+    @POST("/api/v1/auth/login")
+    suspend fun signIn(
+        @Body signInRequest: UserSignInRequest,
+    ): Result<UserSignInResponse>
+
+    @POST("/api/v1/user/join")
+    suspend fun signUp(
+        @Body signUpRequest: UserSignUpRequest,
+    ): Result<Unit>
+}
