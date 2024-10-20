87406e3e33bc0cc8f42bb368e4a0a935524b4a17
Bandal <no1msh1217@gmail.com>
2024-10-20 16:21:11 +0900

    refactor: GoogleSignInButton 에서 로그인 로직을 분리

--- core/auth/src/main/java/com/teamhy2/core/auth/GoogleSignInButton.kt
+++ core/auth/src/main/java/com/teamhy2/core/auth/GoogleSignInButton.kt
@@ -1,7 +1,5 @@
 package com.teamhy2.core.auth
 
-import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,82 +12,23 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.credentials.CredentialManager
-import androidx.credentials.GetCredentialRequest
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.teamhy2.designsystem.ui.theme.Gray800
 import com.teamhy2.designsystem.ui.theme.HY2Theme
 import com.teamhy2.designsystem.ui.theme.HY2Typography
 import com.teamhy2.designsystem.ui.theme.White
-import kotlinx.coroutines.launch
-import java.security.MessageDigest
-import java.util.UUID
 
 @Composable
 fun GoogleSignInButton(
-    onGoogleSignInDone: () -> Unit,
+    onGoogleSignInClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-
-    val onClick: () -> Unit = {
-        val credentialManager = CredentialManager.create(context)
-
-        val rawNonce = UUID.randomUUID().toString()
-        val bytes = rawNonce.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        val hashedNonce = digest.fold("") { str, it -> str + "%02x".format(it) }
-
-        val googleIdOption: GetGoogleIdOption =
-            GetGoogleIdOption.Builder()
-                .setFilterByAuthorizedAccounts(false)
-                .setServerClientId(BuildConfig.WEB_CLIENT_ID)
-                .setNonce(hashedNonce)
-                .build()
-
-        val request: GetCredentialRequest =
-            GetCredentialRequest.Builder()
-                .addCredentialOption(googleIdOption)
-                .build()
-
-        coroutineScope.launch {
-            runCatching {
-                val result =
-                    credentialManager.getCredential(
-                        request = request,
-                        context = context,
-                    )
-                val credential = result.credential
-
-                val googleIdTokenCredential =
-                    GoogleIdTokenCredential
-                        .createFrom(credential.data)
-
-                val googleIdToken = googleIdTokenCredential.idToken
-
-                Log.i("bandal", googleIdToken)
-            }
-                .onSuccess {
-                    Toast.makeText(context, "You are signed in!", Toast.LENGTH_SHORT).show()
-                    onGoogleSignInDone()
-                }.onFailure {
-                    Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
-                }
-        }
-    }
-
     Button(
-        onClick = onClick,
+        onClick = onGoogleSignInClick,
         modifier = modifier.height(50.dp),
         shape = RoundedCornerShape(8.dp),
         contentPadding = PaddingValues(horizontal = 20.dp),
@@ -112,7 +51,7 @@ fun GoogleSignInButton(
 private fun GoogleSignInButtonPreview() {
     HY2Theme {
         GoogleSignInButton(
-            onGoogleSignInDone = {},
+            onGoogleSignInClick = {},
             modifier = Modifier.fillMaxWidth(),
         )
     }
