15219dfe10acfbce75ea7c1f132b42df68e3c638
Bandal <no1msh1217@gmail.com>
2024-10-29 00:47:57 +0900

    refactor: SignUpUiState SealedInterface로 변경

--- onboarding-presentation/src/main/java/com/teamhy2/onboarding/SignUpViewModel.kt
+++ onboarding-presentation/src/main/java/com/teamhy2/onboarding/SignUpViewModel.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,14 +24,7 @@ class SignUpViewModel
         private val userRepository: UserRepository,
     ) : ViewModel() {
         private val _signUpUiState: MutableStateFlow<SignUpUiState> =
-            MutableStateFlow(
-                SignUpUiState(
-                    departments = emptyList(),
-                    isNicknameValidate = false,
-                    nicknameState = NicknameState.NOT_CHECKED,
-                    isDepartmentValidate = false,
-                ),
-            )
+            MutableStateFlow(SignUpUiState.Loading)
         val signUpUiState: StateFlow<SignUpUiState> = _signUpUiState.asStateFlow()
 
         private val _nickname: MutableStateFlow<String> = MutableStateFlow("")
@@ -50,7 +44,13 @@ class SignUpViewModel
 
         fun updateNickname(value: String) {
             _nickname.value = value
-            _signUpUiState.value = _signUpUiState.value.copy(nicknameState = NicknameState.NOT_CHECKED)
+            if (_signUpUiState.value is SignUpUiState.Success) {
+                _signUpUiState.update { currentState ->
+                    (currentState as SignUpUiState.Success).copy(
+                        nicknameState = NicknameState.NOT_CHECKED,
+                    )
+                }
+            }
             checkNicknameValidation()
         }
 
@@ -62,34 +62,48 @@ class SignUpViewModel
         private fun getAllDepartments() {
             viewModelScope.launch {
                 _signUpUiState.value =
-                    _signUpUiState.value.copy(
+                    SignUpUiState.Success(
                         departments = departmentRepository.getAllDepartments(),
+                        isNicknameValidate = false,
+                        nicknameState = NicknameState.NOT_CHECKED,
+                        isDepartmentValidate = false,
                     )
             }
         }
 
         private fun checkNicknameValidation() {
-            _signUpUiState.value =
-                _signUpUiState.value.copy(isNicknameValidate = NicknameValidation.validate(_nickname.value))
+            if (_signUpUiState.value is SignUpUiState.Success) {
+                _signUpUiState.update { currentState ->
+                    (currentState as SignUpUiState.Success).copy(
+                        isNicknameValidate = NicknameValidation.validate(_nickname.value),
+                    )
+                }
+            }
         }
 
         private fun checkDepartmentValidation() {
-            _signUpUiState.value =
-                _signUpUiState.value.copy(
-                    isDepartmentValidate =
-                        _signUpUiState.value.departments.find { it == department.value } != null,
-                )
+            if (_signUpUiState.value is SignUpUiState.Success) {
+                _signUpUiState.update { currentState ->
+                    val success = (currentState as SignUpUiState.Success)
+                    success.copy(
+                        isDepartmentValidate = success.departments.find { it == department.value } != null,
+                    )
+                }
+            }
         }
 
         fun checkNicknameDuplication() {
             viewModelScope.launch {
                 userRepository.checkNicknameDuplication(nickname.value)
                     .onSuccess { isDuplicated ->
-                        if (isDuplicated) {
-                            _signUpUiState.value = _signUpUiState.value.copy(nicknameState = NicknameState.DUPLICATED)
-                            return@onSuccess
+                        if (_signUpUiState.value is SignUpUiState.Success) {
+                            val success = (_signUpUiState.value as SignUpUiState.Success)
+                            if (isDuplicated) {
+                                _signUpUiState.value = success.copy(nicknameState = NicknameState.DUPLICATED)
+                                return@onSuccess
+                            }
+                            _signUpUiState.value = success.copy(nicknameState = NicknameState.NOT_DUPLICATED)
                         }
-                        _signUpUiState.value = _signUpUiState.value.copy(nicknameState = NicknameState.NOT_DUPLICATED)
                     }
                     .onFailure { throwable ->
                         _errorFlow.emit(throwable)
@@ -101,6 +115,7 @@ class SignUpViewModel
             viewModelScope.launch {
                 userRepository.signUp(nickname.value, department.value)
                     .onSuccess {
+                        _signUpUiState.value = SignUpUiState.SignUpDone
                     }
                     .onFailure { throwable ->
                         _errorFlow.emit(throwable)
