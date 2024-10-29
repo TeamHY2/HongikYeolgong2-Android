5cd9269e74383b5c2a94e38e738fc2e4ea894771
librarywon <librarywon@naver.com>
2024-10-29 22:29:15 +0900

    refactor: 컴포넌트 패키지 이동

--- main-presentation/src/main/java/com/teamhy2/feature/home/component/RunningTimerComponent.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/home/component/RunningTimerComponent.kt
@@ -1,4 +1,4 @@
-package com.teamhy2.feature.main.component
+package com.teamhy2.feature.home.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
