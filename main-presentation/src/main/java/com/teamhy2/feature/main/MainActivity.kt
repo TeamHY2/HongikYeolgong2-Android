e6c8c25e675a42a80c261a51e06be69bfa8f9f3a
librarywon <librarywon@naver.com>
2024-11-04 17:11:19 +0900

    refactor: Screen Stack이 계속 쌓이는 현상 해결

--- main-presentation/src/main/java/com/teamhy2/feature/main/MainActivity.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/main/MainActivity.kt
@@ -35,6 +35,7 @@ import com.teamhy2.designsystem.common.HY2LoadingScreen
 import com.teamhy2.designsystem.ui.theme.HY2Theme
 import com.teamhy2.designsystem.util.compositionlocal.LocalShowSnackBar
 import com.teamhy2.designsystem.util.compositionlocal.ShowSnackBar
+import com.teamhy2.feature.home.navigation.Home
 import com.teamhy2.feature.main.component.MainBottomBar
 import com.teamhy2.hongikyeolgong2.main.presentation.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -86,12 +87,11 @@ class MainActivity : AppCompatActivity() {
                                 currentTab = MainTab.fromRoute(currentDestination),
                                 onTabSelected = { tab ->
                                     navController.navigate(tab.route) {
-                                        popUpTo(navController.graph.startDestinationId) {
+                                        popUpTo(Home.ROUTE) {
                                             saveState = true
-                                            inclusive = true
                                         }
-                                        restoreState = true
                                         launchSingleTop = true
+                                        restoreState = true
                                     }
                                 },
                             )
