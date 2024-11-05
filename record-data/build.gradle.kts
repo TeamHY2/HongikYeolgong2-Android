eb3e2e5aee03d33be9887aac0d303b5aa6a59956
librarywon <librarywon@naver.com>
2024-11-05 12:37:48 +0900

    feat: record-data 모듈 생성

---
+++ record-data/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("hongikyeolgong2.android.library")
+}
+
+android {
+    namespace = "com.teamhy2.hongikyeolgong2.record.data"
+}
+
+dependencies {
+    implementation(projects.recordDomain)
+    implementation(projects.core.remote)
+}
