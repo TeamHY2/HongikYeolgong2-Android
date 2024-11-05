0d4280059ed22f489749c8e48f532b761604b541
Bandal <no1msh1217@gmail.com>
2024-11-05 22:01:14 +0900

    feat: 토큰의 유효성을 판단하는 객체 구현

---
+++ core/remote/src/main/java/com/benenfeldt/remote/token/TokenValidator.kt
@@ -0,0 +1,17 @@
+package com.benenfeldt.remote.token
+
+import com.benenfeldt.remote.api.TokenService
+import com.benenfeldt.remote.mapper.toResult
+import com.benenfeldt.remote.mapper.toTokenInformation
+import javax.inject.Inject
+
+class TokenValidator
+    @Inject
+    constructor(
+        private val tokenService: TokenService,
+    ) {
+        suspend fun validate(): Result<TokenInformation> {
+            return tokenService.getTokenInformation()
+                .toResult { it.data.toTokenInformation() }
+        }
+    }
