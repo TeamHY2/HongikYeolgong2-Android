23fa2e214eeccd74a372a7067dabdb50347d44c8
librarywon <librarywon@naver.com>
2024-11-06 00:21:19 +0900

    refactor: lint 적용

--- record-data/src/main/java/com/teamhy2/record/data/mapper/StudyDurationMapper.kt
+++ record-data/src/main/java/com/teamhy2/record/data/mapper/StudyDurationMapper.kt
@@ -3,7 +3,7 @@ package com.teamhy2.record.data.mapper
 import com.benenfeldt.remote.dto.StudyDurationResponse
 import com.teamhy2.record.domain.model.StudyDuration
 
-fun StudyDurationResponse.toDomain(): StudyDuration  {
+fun StudyDurationResponse.toDomain(): StudyDuration {
     return StudyDuration(
         yearHours = yearHours,
         yearMinutes = yearMinutes,
