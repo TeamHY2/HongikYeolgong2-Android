fdff5632e9ca0431eccaa289243dd17da1793a5e
librarywon <librarywon@naver.com>
2024-11-05 17:31:59 +0900

    feat: calendarStudyDay data 로직 구현

---
+++ record-data/src/main/java/com/teamhy2/record/data/repository/RemoteCalendarStudyDayRepository.kt
@@ -0,0 +1,41 @@
+package com.teamhy2.record.data.repository
+
+import com.benenfeldt.remote.api.StudyService
+import com.hongikyeolgong2.calendar.model.StudyDay
+import com.teamhy2.record.data.mapper.toDomain
+import com.teamhy2.record.domain.repository.CalendarStudyDayRepository
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.time.LocalDate
+import java.time.YearMonth
+import javax.inject.Inject
+
+class RemoteCalendarStudyDayRepository
+    @Inject
+    constructor(
+        private val studyService: StudyService,
+    ) : CalendarStudyDayRepository {
+        private val cachedStudyDays: MutableMap<YearMonth, List<StudyDay>> = mutableMapOf()
+        private val cacheMutex: Mutex = Mutex()
+
+        override suspend fun updateCalendarStudyDay(): Result<Unit> {
+            return studyService.getCalendarStudyDay().map { response ->
+                val studyDays: List<StudyDay> = response.data.map { it.toDomain() }
+                cacheMutex.withLock {
+                    cachedStudyDays.clear()
+                    studyDays.forEach { studyDay ->
+                        val yearMonth: YearMonth = YearMonth.from(studyDay.date)
+                        cachedStudyDays[yearMonth] =
+                            cachedStudyDays.getOrDefault(yearMonth, emptyList()) + studyDay
+                    }
+                }
+            }
+        }
+
+        override suspend fun fetchStudyDaysForYearMonth(date: LocalDate): List<StudyDay> {
+            val yearMonth: YearMonth = YearMonth.of(date.year, date.month)
+            return cacheMutex.withLock {
+                cachedStudyDays[yearMonth] ?: emptyList()
+            }
+        }
+    }
---
+++ record-domain/src/main/java/com/teamhy2/record/domain/repository/CalendarStudyDayRepository.kt
@@ -0,0 +1,10 @@
+package com.teamhy2.record.domain.repository
+
+import com.hongikyeolgong2.calendar.model.StudyDay
+import java.time.LocalDate
+
+interface CalendarStudyDayRepository {
+    suspend fun updateCalendarStudyDay(): Result<Unit>
+
+    suspend fun fetchStudyDaysForYearMonth(date: LocalDate): List<StudyDay>
+}
