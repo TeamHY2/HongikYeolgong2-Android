6793c2977a6b34e77541cd6ee20600847ea30b28
librarywon <librarywon@naver.com>
2024-11-12 16:34:01 +0900

    feat: 열람실 연장시 이전 이벤트를 cancel하고 다시 시작하는 로직 구현

--- main-presentation/src/main/java/com/teamhy2/feature/home/HomeScreen.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/home/HomeScreen.kt
@@ -122,7 +122,12 @@ fun HomeRoute(
                         homeViewModel.updateStudyRoomExtendDialogVisibility(false)
                     },
                     onRightButtonClick = {
-                        homeViewModel.updateStudyRoomExtendDialogVisibility(false)
+                        homeViewModel.run {
+                            updateStudyRoomExtendDialogVisibility(false)
+                            saveStudyDay(true)
+                            updateTimerRunning(true)
+                            updateSelectedTime(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES))
+                        }
                         startTimer(
                             LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES),
                             homeViewModel,
@@ -147,7 +152,7 @@ fun HomeRoute(
                     onRightButtonClick = {
                         homeViewModel.updateStudyRoomEndDialogVisibility(false)
                         homeViewModel.updateTimerRunning(false)
-                        homeViewModel.saveStudyDay()
+                        homeViewModel.saveStudyDay(false)
                     },
                     onDismiss = {
                         homeViewModel.updateStudyRoomEndDialogVisibility(false)
@@ -215,7 +220,7 @@ private fun startTimer(
                 },
                 Timer.TIME_OVER_SECONDS to {
                     homeViewModel.updateTimerRunning(false)
-                    homeViewModel.saveStudyDay()
+                    homeViewModel.saveStudyDay(false)
                 },
             ),
     )
