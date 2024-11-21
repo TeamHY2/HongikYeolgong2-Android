443e925436a4ae57c0ed1b10bc64a9dca89eb2b7
librarywon <librarywon@naver.com>
2024-11-21 15:34:48 +0900

    refactor: duration이 시간을 기준으로 나오도록 수정

--- timer-presentation/src/main/java/com/teamhy2/hongikyeolgong2/timer/presentation/TimerViewModel.kt
+++ timer-presentation/src/main/java/com/teamhy2/hongikyeolgong2/timer/presentation/TimerViewModel.kt
@@ -37,7 +37,7 @@ class TimerViewModel
         private fun getStudyRoomDuration() {
             viewModelScope.launch {
                 val hours = timerRepository.getStudyRoomHourDuration()
-                _durationHour.value = Duration.ofMinutes(hours)
+                _durationHour.value = Duration.ofHours(hours)
             }
         }
 
