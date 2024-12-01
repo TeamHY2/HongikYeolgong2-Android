9b9be806d28081ed6bf7e4f9bd0974fa5416f5b7
librarywon <librarywon@naver.com>
2024-12-01 16:30:02 +0900

    feat: TimerUiModel에 startDateTime 추가

--- timer-presentation/src/main/java/com/teamhy2/hongikyeolgong2/timer/presentation/model/TimerUiModel.kt
+++ timer-presentation/src/main/java/com/teamhy2/hongikyeolgong2/timer/presentation/model/TimerUiModel.kt
@@ -1,6 +1,9 @@
 package com.teamhy2.hongikyeolgong2.timer.presentation.model
 
+import java.time.LocalDateTime
+
 data class TimerUiModel(
+    val startDateTime: LocalDateTime = LocalDateTime.now(),
     val startTime: String = "",
     val startTimeMeridiem: String = "",
     val endTime: String = "",
