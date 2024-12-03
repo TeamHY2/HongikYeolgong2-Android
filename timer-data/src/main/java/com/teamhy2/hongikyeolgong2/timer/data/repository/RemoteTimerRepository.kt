e814b60fe5ee8b8abc7fbde942d16ad30a140b63
Bandal <no1msh1217@gmail.com>
2024-12-03 18:30:08 +0900

    feat: 타이머 사용시간을 로컬에 저장할 수 있다

--- timer-data/src/main/java/com/teamhy2/hongikyeolgong2/timer/data/repository/RemoteTimerRepository.kt
+++ timer-data/src/main/java/com/teamhy2/hongikyeolgong2/timer/data/repository/RemoteTimerRepository.kt
@@ -2,15 +2,45 @@ package com.teamhy2.hongikyeolgong2.timer.data.repository
 
 import com.benenfeldt.remote.api.LibraryService
 import com.benenfeldt.remote.mapper.toResult
+import com.teamhy2.hongikyeolgong2.timer.model.TimerDataSource
+import com.teamhy2.hongikyeolgong2.timer.model.TimerDuration
 import com.teamhy2.hongikyeolgong2.timer.model.TimerRepository
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 class RemoteTimerRepository
     @Inject
     constructor(
         private val libraryService: LibraryService,
+        private val timerDataSource: TimerDataSource,
     ) : TimerRepository {
-        override suspend fun getStudyRoomHourDuration(): Result<Int> {
-            return libraryService.getStudyRoomHourDuration().toResult { it.data.libraryHours }
+        override suspend fun getStudyRoomHourDuration(): Int {
+            val studyRoomHourDuration =
+                libraryService.getStudyRoomHourDuration()
+                    .toResult { it.data.libraryHours }
+                    .getOrDefault(TimerRepository.MINIMUM_STUDY_ROOM_HOUR_DURATION)
+
+            return studyRoomHourDuration
+        }
+
+        override suspend fun getCurrentTimerDuration(): TimerDuration? {
+            val startTime: LocalDateTime = timerDataSource.getStartTime() ?: return null
+            val endTime: LocalDateTime = timerDataSource.getEndTime() ?: return null
+
+            if (endTime.isBefore(LocalDateTime.now())) {
+                timerDataSource.clearTimes()
+                return null
+            }
+
+            return TimerDuration(startTime, endTime)
+        }
+
+        override suspend fun setCurrentTimerDuration(timerDuration: TimerDuration) {
+            timerDataSource.saveStartTime(timerDuration.startTime.toString())
+            timerDataSource.saveEndTime(timerDuration.endTime.toString())
+        }
+
+        override suspend fun clearCurrentTimerDuration() {
+            timerDataSource.clearTimes()
         }
     }
