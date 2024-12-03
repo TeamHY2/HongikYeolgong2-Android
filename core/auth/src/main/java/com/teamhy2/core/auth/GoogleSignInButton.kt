74d20e4a5fbb5a53854ba7c1f69f49125ebbfe04
librarywon <librarywon@naver.com>
2024-12-03 16:28:53 +0900

    feat: Button -> ThrottledButton 교체

--- core/auth/src/main/java/com/teamhy2/core/auth/GoogleSignInButton.kt
+++ core/auth/src/main/java/com/teamhy2/core/auth/GoogleSignInButton.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,25 +16,35 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.teamhy2.designsystem.common.ThrottledButton
 import com.teamhy2.designsystem.ui.theme.Gray800
 import com.teamhy2.designsystem.ui.theme.HY2Theme
 import com.teamhy2.designsystem.ui.theme.HY2Typography
 import com.teamhy2.designsystem.ui.theme.White
+import com.teamhy2.designsystem.util.modifier.throttleClick
 
 @Composable
 fun GoogleSignInButton(
     onGoogleSignInClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Button(
+    ThrottledButton(
         onClick = onGoogleSignInClick,
-        modifier = modifier.height(50.dp),
+        modifier = modifier
+            .height(50.dp)
+            .throttleClick {
+                onGoogleSignInClick()
+            },
         shape = RoundedCornerShape(8.dp),
         contentPadding = PaddingValues(horizontal = 20.dp),
         colors = ButtonDefaults.buttonColors().copy(containerColor = White),
     ) {
         Box(modifier = Modifier.weight(1f)) {
-            Image(painter = painterResource(id = R.drawable.logo_google), contentDescription = null, modifier = Modifier.size(24.dp))
+            Image(
+                painter = painterResource(id = R.drawable.logo_google),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
         }
         Text(
             text = stringResource(R.string.google_sign_in_text),
