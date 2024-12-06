e44350f172403fbb700e87713e971ccf557e5852
librarywon <librarywon@naver.com>
2024-12-06 16:40:26 +0900

    feat: PromotionRepository 구현

---
+++ main-data/src/main/java/com/teamhy2/main/data/repository/DefaultPromotionRepository.kt
@@ -0,0 +1,30 @@
+package com.teamhy2.main.data.repository
+
+import com.teamhy2.main.data.datastore.PromotionDataStore
+import com.teamhy2.main.domain.datasource.PromotionDataSource
+import com.teamhy2.main.domain.model.Promotion
+import com.teamhy2.main.domain.repository.PromotionRepository
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+import javax.inject.Inject
+
+class DefaultPromotionRepository
+    @Inject
+    constructor(
+        private val dataSource: PromotionDataSource,
+        private val dataStore: PromotionDataStore,
+    ) : PromotionRepository {
+        override val isPromotionDismissed: Flow<Boolean>
+            get() = dataStore.isPromotionDismissed
+
+        override suspend fun fetchPromotionData(): Result<Promotion> {
+            return dataSource.fetchPromotionData()
+        }
+
+        override suspend fun savePromotionDismissPeriod(
+            startDate: LocalDate,
+            endDate: LocalDate,
+        ) {
+            dataStore.savePromotionDismissPeriod(startDate, endDate)
+        }
+    }
---
+++ main-domain/src/main/java/com/teamhy2/main/domain/repository/PromotionRepository.kt
@@ -0,0 +1,16 @@
+package com.teamhy2.main.domain.repository
+
+import com.teamhy2.main.domain.model.Promotion
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+interface PromotionRepository {
+    val isPromotionDismissed: Flow<Boolean>
+
+    suspend fun fetchPromotionData(): Result<Promotion>
+
+    suspend fun savePromotionDismissPeriod(
+        startDate: LocalDate,
+        endDate: LocalDate,
+    )
+}
