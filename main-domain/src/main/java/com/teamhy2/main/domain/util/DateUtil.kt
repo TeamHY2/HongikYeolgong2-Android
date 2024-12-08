1427e0c36c4c7ba214e62e71830a3cfdcbee1e1d
librarywon <librarywon@naver.com>
2024-12-08 22:13:30 +0900

    refactor: requireNotNull 로직 제거, isTodayWithinDateRange 로직 개선

--- main-domain/src/main/java/com/teamhy2/main/domain/util/DateUtil.kt
+++ main-domain/src/main/java/com/teamhy2/main/domain/util/DateUtil.kt
@@ -1,21 +1,41 @@
 package com.teamhy2.main.domain.util
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object DateUtil {
     /**
      * Checks if the current date is within the specified date range.
      *
-     * @param startDate The start date of the range.
-     * @param endDate The end date of the range.
+     * @param startDateString The start date string of the range.
+     * @param endDateString The end date string of the range.
      * @return True if the current date is within the range, false otherwise.
      */
     fun isTodayWithinDateRange(
-        startDate: LocalDate,
-        endDate: LocalDate,
+        startDateString: String,
+        endDateString: String,
     ): Boolean {
-        val today: LocalDate = LocalDate.now()
-        return today.isEqual(startDate) || today.isEqual(endDate) ||
-            (today.isAfter(startDate) && today.isBefore(endDate))
+        val formatter = DateTimeFormatter.ofPattern("[yyyy-MM-dd][yyyy-M-d]")
+
+        val startDateResult =
+            runCatching {
+                LocalDate.parse(startDateString, formatter)
+            }
+
+        val endDateResult =
+            runCatching {
+                LocalDate.parse(endDateString, formatter)
+            }
+
+        if (startDateResult.isFailure || endDateResult.isFailure) {
+            return false
+        }
+
+        val startDate = startDateResult.getOrNull()
+        val endDate = endDateResult.getOrNull()
+        val todayDate: LocalDate = LocalDate.now()
+
+        return todayDate.isEqual(startDate) || todayDate.isEqual(endDate) ||
+            (todayDate.isAfter(startDate) && todayDate.isBefore(endDate))
     }
 }
