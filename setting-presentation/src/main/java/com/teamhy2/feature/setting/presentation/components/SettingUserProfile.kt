7ececd780c68f487df1c14ddf26d5d4d376b6e0b
Bandal <no1msh1217@gmail.com>
2025-01-12 16:11:48 +0900

    feat: 설정화면 디자인 변경 사항 수정

--- setting-presentation/src/main/java/com/teamhy2/feature/setting/presentation/components/SettingUserProfile.kt
+++ setting-presentation/src/main/java/com/teamhy2/feature/setting/presentation/components/SettingUserProfile.kt
@@ -1,11 +1,13 @@
 package com.teamhy2.feature.setting.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,9 +16,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import com.teamhy2.designsystem.ui.theme.Gray200
-import com.teamhy2.designsystem.ui.theme.Gray400
+import com.teamhy2.designsystem.ui.theme.Gray100
 import com.teamhy2.designsystem.ui.theme.HY2Typography
+import com.teamhy2.designsystem.util.modifier.throttleClickable
+import com.teamhy2.hongikyeolgong2.setting.presentation.R
 import com.teamhy2.hongikyeolgong2.setting.presentation.R.drawable.img_settting_profile
 import com.teamhy2.user.domain.model.UserInfo
 
@@ -25,6 +28,7 @@ private const val PROFILE_IMAGE_SIZE = 56
 @Composable
 fun SettingUserProfile(
     userInfo: UserInfo,
+    onProfileClick: () -> Unit,
     modifier: Modifier = Modifier,
     profileImageUrl: String = "",
 ) {
@@ -35,36 +39,34 @@ fun SettingUserProfile(
             painterResource(id = img_settting_profile)
         }
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.fillMaxWidth(),
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth().throttleClickable(onClick = onProfileClick),
     ) {
-        Spacer(modifier = Modifier.width(1.dp))
         Image(
             painter = profileImagePainter,
             contentDescription = null,
             modifier = Modifier.size(PROFILE_IMAGE_SIZE.dp),
         )
-        Spacer(modifier = Modifier.width(20.dp))
-        Row {
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
             Text(
                 text = userInfo.nickname,
-                style = HY2Typography().body05,
-                color = Gray200,
+                style = HY2Typography().title02,
+                color = Gray100,
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = "|",
-                style = HY2Typography().body05,
-                color = Gray400,
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = userInfo.department,
-                style = HY2Typography().body05,
-                color = Gray200,
+            Icon(
+                painterResource(id = R.drawable.ic_arrow_right),
+                contentDescription = null,
+                tint = Gray100,
             )
         }
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            text = userInfo.department,
+            style = HY2Typography().body05,
+            color = Gray100,
+        )
     }
 }
 
@@ -77,5 +79,8 @@ private fun SettingUserProfilePreview() {
             email = "urim@gmail.com",
             department = "디자인컨버전스학부",
         )
-    SettingUserProfile(userInfo)
+    SettingUserProfile(
+        userInfo = userInfo,
+        onProfileClick = {},
+    )
 }
