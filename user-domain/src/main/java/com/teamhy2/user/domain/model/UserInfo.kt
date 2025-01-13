d57b726ee5c33bdd10d88e9ee46167a48a07646d
Bandal <no1msh1217@gmail.com>
2025-01-13 23:59:53 +0900

    feat: LocalNavController 구현

--- user-domain/src/main/java/com/teamhy2/user/domain/model/UserInfo.kt
+++ user-domain/src/main/java/com/teamhy2/user/domain/model/UserInfo.kt
@@ -4,4 +4,8 @@ data class UserInfo(
     val nickname: String,
     val email: String,
     val department: String,
-)
+) {
+    companion object {
+        val LOADING = UserInfo("불러오는 중..", "불러오는 중..", "불러오는 중..")
+    }
+}
