5c062da65e2df5ab240365a00c4c166b6b3f4ee7
Bandal <no1msh1217@gmail.com>
2025-02-23 00:10:56 +0900

    feat: 기록화면 캘린더 날짜 컴포넌트를 요구사항에 맞춰 변경

--- calendar-presentation/src/main/java/com/hongikyeolgong2/calendar/presentation/Day.kt
+++ calendar-presentation/src/main/java/com/hongikyeolgong2/calendar/presentation/Day.kt
@@ -2,43 +2,44 @@ package com.hongikyeolgong2.calendar.presentation
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hongikyeolgong2.calendar.model.StudyDay
 import com.hongikyeolgong2.calendar.model.StudyRoomUsage
-import com.teamhy2.designsystem.ui.theme.Blue200
-import com.teamhy2.designsystem.ui.theme.Blue400
 import com.teamhy2.designsystem.ui.theme.Gray100
 import com.teamhy2.designsystem.ui.theme.Gray300
 import com.teamhy2.designsystem.ui.theme.Gray600
 import com.teamhy2.designsystem.ui.theme.HY2Theme
 import com.teamhy2.designsystem.ui.theme.HY2Typography
 import com.teamhy2.designsystem.ui.theme.White
+import com.teamhy2.designsystem.util.modifier.throttleClickable
 import com.teamhy2.hongikyeolgong2.calendar.presentation.R.drawable.bg_day_0
 import com.teamhy2.hongikyeolgong2.calendar.presentation.R.drawable.bg_day_1
 import com.teamhy2.hongikyeolgong2.calendar.presentation.R.drawable.bg_day_2
 import com.teamhy2.hongikyeolgong2.calendar.presentation.R.drawable.bg_day_3
 import java.time.LocalDate
 
-private const val DEFAULT_RADIUS = 8
-private const val DAY_SIZE_RATIO = 1.212f
+private const val DAY_SIZE_RATIO = 1f
 
 @Composable
 fun Day(
     studyDay: StudyDay,
+    dayState: DayState,
+    onDayClicked: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val dayUi = DayUi.of(studyDay.studyRoomUsage)
@@ -46,15 +47,22 @@ fun Day(
     Box(
         modifier =
             modifier
-                .aspectRatio(DAY_SIZE_RATIO, false)
-                .then(dayUi.modifier),
+                .aspectRatio(DAY_SIZE_RATIO)
+                .alpha(
+                    when (dayState) {
+                        DayState.Default, DayState.Selected -> 1f
+                        DayState.Unselected -> dayUi.unselectedAlpha
+                    },
+                )
+                .throttleClickable(onClick = onDayClicked),
         contentAlignment = Alignment.Center,
     ) {
         Image(
             painter = painterResource(id = dayUi.background),
             contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.matchParentSize(),
+            modifier =
+                Modifier
+                    .fillMaxSize(),
         )
         Text(
             text = studyDay.date.dayOfMonth.toString(),
@@ -64,49 +72,36 @@ fun Day(
     }
 }
 
+enum class DayState {
+    Default,
+    Selected,
+    Unselected,
+}
+
 enum class DayUi(
     val textColor: Color,
     @DrawableRes val background: Int,
-    val modifier: Modifier,
+    val unselectedAlpha: Float,
 ) {
     NEVER_USED(
         textColor = Gray300,
         background = bg_day_0,
-        modifier =
-            Modifier
-                .clip(RoundedCornerShape(DEFAULT_RADIUS.dp)),
+        unselectedAlpha = 0.3f,
     ),
     USED_ONCE(
         textColor = Gray100,
         background = bg_day_1,
-        modifier =
-            Modifier
-                .clip(RoundedCornerShape(DEFAULT_RADIUS.dp))
-                .border(
-                    width = 1.dp,
-                    color = Blue400,
-                    shape = RoundedCornerShape(DEFAULT_RADIUS.dp),
-                ),
+        unselectedAlpha = 0.2f,
     ),
     USED_ONCE_EXTENDED_ONCE(
         textColor = White,
         background = bg_day_2,
-        modifier =
-            Modifier
-                .clip(RoundedCornerShape(DEFAULT_RADIUS.dp))
-                .border(width = 1.dp, color = Blue200, shape = RoundedCornerShape(DEFAULT_RADIUS.dp)),
+        unselectedAlpha = 0.1f,
     ),
     USED_ONCE_EXTENDED_TWICE(
         textColor = Gray600,
         background = bg_day_3,
-        modifier =
-            Modifier
-                .clip(RoundedCornerShape(DEFAULT_RADIUS.dp))
-                .border(
-                    width = 1.dp,
-                    color = Color(0xFFCECF37),
-                    shape = RoundedCornerShape(DEFAULT_RADIUS.dp),
-                ),
+        unselectedAlpha = 0.05f,
     ),
     ;
 
@@ -126,10 +121,21 @@ enum class DayUi(
 @Composable
 private fun NeverUsedDayPreview() {
     HY2Theme {
-        Day(
-            studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.NEVER_USED),
-            modifier = Modifier.size(40.dp),
-        )
+        Row {
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.NEVER_USED),
+                dayState = DayState.Default,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+            Spacer(Modifier.width(12.dp))
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.NEVER_USED),
+                dayState = DayState.Unselected,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+        }
     }
 }
 
@@ -137,10 +143,21 @@ private fun NeverUsedDayPreview() {
 @Composable
 private fun UsedOnceDayPreview() {
     HY2Theme {
-        Day(
-            studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE),
-            modifier = Modifier.size(40.dp),
-        )
+        Row {
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE),
+                dayState = DayState.Default,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+            Spacer(Modifier.width(12.dp))
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE),
+                dayState = DayState.Unselected,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+        }
     }
 }
 
@@ -148,10 +165,21 @@ private fun UsedOnceDayPreview() {
 @Composable
 private fun UsedOnceExtendedOnceDayPreview() {
     HY2Theme {
-        Day(
-            studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE_EXTENDED_ONCE),
-            modifier = Modifier.size(40.dp),
-        )
+        Row {
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE_EXTENDED_ONCE),
+                dayState = DayState.Default,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+            Spacer(Modifier.width(12.dp))
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE_EXTENDED_ONCE),
+                dayState = DayState.Unselected,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+        }
     }
 }
 
@@ -159,9 +187,20 @@ private fun UsedOnceExtendedOnceDayPreview() {
 @Composable
 private fun UsedOnceExtendedTwiceDayPreview() {
     HY2Theme {
-        Day(
-            studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE_EXTENDED_TWICE),
-            modifier = Modifier.size(40.dp),
-        )
+        Row {
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE_EXTENDED_TWICE),
+                dayState = DayState.Default,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+            Spacer(Modifier.width(12.dp))
+            Day(
+                studyDay = StudyDay(LocalDate.now(), StudyRoomUsage.USED_ONCE_EXTENDED_TWICE),
+                dayState = DayState.Unselected,
+                onDayClicked = {},
+                modifier = Modifier.size(40.dp),
+            )
+        }
     }
 }
