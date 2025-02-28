899351c575fa6a9641ea8a5e726f09296b5c828e
librarywon <librarywon@naver.com>
2025-02-28 12:08:42 +0900

    refactor: RecordShareImageManager 주석 제거

--- record-presentation/src/main/java/com/teamhy2/record/share/RecordShareImageManager.kt
+++ record-presentation/src/main/java/com/teamhy2/record/share/RecordShareImageManager.kt
@@ -1,9 +1,5 @@
 package com.teamhy2.record.share
 
-/**
- * RecordShareImageManager는 이미지 저장(ImageSaver)과 공유(ImageSharer) 기능을
- * 캡슐화한 싱글톤 객체입니다.
- */
 object RecordShareImageManager {
     val imageSaver: ImageSaver = MediaStoreImageSaver
     val imageSharer: ImageSharer = InstagramImageSharer
