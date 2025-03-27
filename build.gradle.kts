4c487787467255ac948c3d8aa06fb3a2713178e2
Bandal <no1msh1217@gmail.com>
2025-03-27 14:58:57 +0900

    feat: KSP로 마이그레이션 및 Hilt 버전 업그레이드
    - KSP를 지원하는 Hilt 버전으로 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
+    alias(libs.plugins.ksp) apply false
 }
 
 allprojects {
