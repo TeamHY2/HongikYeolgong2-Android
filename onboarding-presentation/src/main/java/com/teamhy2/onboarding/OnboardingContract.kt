cf24db4fac5614800ccc5c1a78a6c7d088e39595
librarywon <librarywon@naver.com>
2025-04-20 16:19:02 +0900

    refactor: 온보딩 화면 MVI 리펙토링

---
+++ onboarding-presentation/src/main/java/com/teamhy2/onboarding/OnboardingContract.kt
@@ -0,0 +1,13 @@
+package com.teamhy2.onboarding
+
+data object OnboardingState
+
+sealed interface OnboardingSideEffect {
+    data class ShowError(val errorMessage: String) : OnboardingSideEffect
+
+    sealed interface Navigation : OnboardingSideEffect {
+        data object SignIn : Navigation
+
+        data object Home : Navigation
+    }
+}
