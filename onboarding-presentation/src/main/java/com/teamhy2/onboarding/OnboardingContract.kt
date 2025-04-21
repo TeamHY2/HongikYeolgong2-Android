752da7efa221f23b9d4d8be1d64fa09406f60ec4
librarywon <librarywon@naver.com>
2025-04-21 22:24:03 +0900

    refactor: 스낵바 엘비스 연산자 제거

--- onboarding-presentation/src/main/java/com/teamhy2/onboarding/OnboardingContract.kt
+++ onboarding-presentation/src/main/java/com/teamhy2/onboarding/OnboardingContract.kt
@@ -3,7 +3,7 @@ package com.teamhy2.onboarding
 data object OnboardingState
 
 sealed interface OnboardingSideEffect {
-    data class ShowError(val errorMessage: String) : OnboardingSideEffect
+    data class ShowError(val errorMessage: String?) : OnboardingSideEffect
 
     sealed interface Navigation : OnboardingSideEffect {
         data object SignIn : Navigation
