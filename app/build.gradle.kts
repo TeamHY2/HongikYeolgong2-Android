76fef4d36798a3c60b2486041b62cdbe8fbd0b6a
librarywon <librarywon@naver.com>
2025-05-24 19:54:57 +0900

    infra: BaselineProfile 생성파일 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,12 +45,6 @@ android {
     }
 }
 
-tasks.configureEach {
-    if (name.contains("baselineProfile") && System.getenv("CI") == "true") {
-        enabled = false
-    }
-}
-
 dependencies {
     implementation(projects.core.notification)
     implementation(projects.core.designsystem)
