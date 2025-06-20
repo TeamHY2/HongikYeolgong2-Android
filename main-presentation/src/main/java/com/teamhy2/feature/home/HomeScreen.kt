dd3e69e5abd2c3ac22888f9dff669e4d862d7569
Bandal <no1msh1217@gmail.com>
2025-06-20 16:30:29 +0900

    feat: HomeScreen에서 FocusMode 진입 버튼 구현

--- main-presentation/src/main/java/com/teamhy2/feature/home/HomeScreen.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/home/HomeScreen.kt
@@ -205,6 +205,9 @@ fun HomeRoute(
                 onStudyRoomEndClick = {
                     isStudyRoomEndDialog = true
                 },
+                onFocusModeClick = {
+                    localNavController.navigateToFocusMode()
+                },
                 modifier = modifier,
             )
         }
@@ -231,6 +234,7 @@ fun HomeScreen(
     onStudyRoomStartClick: () -> Unit,
     onStudyRoomExtendClick: () -> Unit,
     onStudyRoomEndClick: () -> Unit,
+    onFocusModeClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
@@ -252,6 +256,7 @@ fun HomeScreen(
             onStudyRoomStartClick = onStudyRoomStartClick,
             onStudyRoomExtendClick = onStudyRoomExtendClick,
             onStudyRoomEndClick = onStudyRoomEndClick,
+            onFocusModeClick = onFocusModeClick,
             modifier = Modifier.padding(horizontal = 24.dp),
         )
     }
@@ -279,6 +284,7 @@ private fun HomeBody(
     onStudyRoomStartClick: () -> Unit,
     onStudyRoomExtendClick: () -> Unit,
     onStudyRoomEndClick: () -> Unit,
+    onFocusModeClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
@@ -293,6 +299,7 @@ private fun HomeBody(
                     timerUiState = timerUiState,
                     onStudyRoomExtendClick = onStudyRoomExtendClick,
                     onStudyRoomEndClick = onStudyRoomEndClick,
+                    onFocusModeClick = onFocusModeClick,
                 )
                 Spacer(modifier = Modifier.weight(1f))
             }
@@ -321,6 +328,7 @@ private fun HomeScreenPreview() {
             onStudyRoomStartClick = { },
             onStudyRoomExtendClick = { },
             onStudyRoomEndClick = { },
+            onFocusModeClick = { },
         )
     }
 }
