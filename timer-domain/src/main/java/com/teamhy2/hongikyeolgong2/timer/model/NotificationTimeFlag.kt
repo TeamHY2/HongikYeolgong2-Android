ad8c618f040e3b8e3b2e68360ca9f10eea40a929
Bandal <no1msh1217@gmail.com>
2025-07-09 13:07:08 +0900

    refactor: enum 값들 이름 변경

--- timer-domain/src/main/java/com/teamhy2/hongikyeolgong2/timer/model/NotificationTimeFlag.kt
+++ timer-domain/src/main/java/com/teamhy2/hongikyeolgong2/timer/model/NotificationTimeFlag.kt
@@ -2,6 +2,6 @@ package com.teamhy2.hongikyeolgong2.timer.model
 
 enum class NotificationTimeFlag(val millis: Long) {
     FINISH_TIME(0L),
-    TEN_MINUTES_MS(600_000L),
-    THIRTY_MINUTES_MS(1_800_000L),
+    TEN_MINUTES(600_000L),
+    THIRTY_MINUTES(1_800_000L),
 }
