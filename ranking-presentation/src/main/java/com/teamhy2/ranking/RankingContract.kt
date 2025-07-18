031900629162b3888509eee4eb7525cbaa3dd331
Bandal <no1msh1217@gmail.com>
2025-07-18 23:58:45 +0900

    refactor: RankingContract 재정의

--- ranking-presentation/src/main/java/com/teamhy2/ranking/RankingContract.kt
+++ ranking-presentation/src/main/java/com/teamhy2/ranking/RankingContract.kt
@@ -1,5 +1,8 @@
 package com.teamhy2.ranking
 
+import com.teamhy2.designsystem.util.mvi.SideEffect
+import com.teamhy2.designsystem.util.mvi.UiIntent
+import com.teamhy2.designsystem.util.mvi.UiState
 import com.teamhy2.ranking.model.DepartmentRanking
 
 data class RankingState(
@@ -7,8 +10,16 @@ data class RankingState(
     val currentWeek: String = "",
     val departmentRankings: List<DepartmentRanking> = emptyList(),
     val isNextWeekEnabled: Boolean = false,
-)
+) : UiState
 
-sealed interface RankingSideEffect {
+sealed interface RankingIntent : UiIntent {
+    data object EnterRankingScreen : RankingIntent
+
+    data object MoveToNextMonth : RankingIntent
+
+    data object MoveToPreviousMonth : RankingIntent
+}
+
+sealed interface RankingSideEffect : SideEffect {
     data class ShowError(val throwable: Throwable) : RankingSideEffect
 }
