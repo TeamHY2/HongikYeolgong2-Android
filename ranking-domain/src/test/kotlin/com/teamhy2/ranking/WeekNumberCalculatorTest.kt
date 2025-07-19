1be31af708422c845a3928ed7693149dd08aab7a
Bandal <no1msh1217@gmail.com>
2025-07-19 16:19:24 +0900

    refactor: WeekNumberCalculator가 자신의 상태를 직접 관리하도록 변경

--- ranking-domain/src/test/kotlin/com/teamhy2/ranking/WeekNumberCalculatorTest.kt
+++ ranking-domain/src/test/kotlin/com/teamhy2/ranking/WeekNumberCalculatorTest.kt
@@ -5,56 +5,88 @@ import io.kotest.matchers.shouldBe
 import java.time.LocalDate
 
 class WeekNumberCalculatorTest : BehaviorSpec({
-    Given("날짜(LocalDate)가 주어지면 weekNumber를 계산할 수 있다") {
-        When("날짜가 2025년 1월 6일(월요일)인 경우") {
-            val date: LocalDate = LocalDate.of(2025, 1, 6)
-            val weekNumber: Int = WeekNumberCalculator.calculateWeekNumber(date)
+    Given("날짜가 2025년 1월 6일(월요일)인 경우") {
+        val date: LocalDate = LocalDate.of(2025, 1, 6)
+
+        When("현재 주 번호를 계산하면") {
+            val weekNumberCalculator = WeekNumberCalculator(date)
+            val actualWeekNumber: Int = weekNumberCalculator.currentWeekNumber
 
             Then("주 번호는 202502가 되어야 한다") {
-                weekNumber shouldBe 202502
-            }
-        }
-
-        When("날짜가 2025년 12월 31일(수요일)인 경우") {
-            val date: LocalDate = LocalDate.of(2025, 12, 31)
-            val weekNumber: Int = WeekNumberCalculator.calculateWeekNumber(date)
-
-            Then("주 번호는 202601이 되어야 한다") {
-                weekNumber shouldBe 202601
+                actualWeekNumber shouldBe 202502
             }
         }
     }
 
-    Given("현재 weekNumber를 기반으로 다음주 or 저번주 weekNumber를 계산할 수 있다") {
-        When("202502에서 +1 주 이동하면") {
-            val shiftedWeek: Int = WeekNumberCalculator.shiftWeekNumber(202502, 1)
+    Given("날짜가 2025년 12월 31일(수요일)인 경우") {
+        val date: LocalDate = LocalDate.of(2025, 12, 31)
 
-            Then("202503이 되어야 한다") {
-                shiftedWeek shouldBe 202503
+        When("현재 주 번호를 계산하면") {
+            val weekNumberCalculator = WeekNumberCalculator(date)
+            val actualWeekNumber: Int = weekNumberCalculator.currentWeekNumber
+
+            Then("주 번호는 202601이 되어야 한다") {
+                actualWeekNumber shouldBe 202601
+            }
+        }
+    }
+
+    Given("날짜가 2025년 1월 6일로 주 번호가 202502이고") {
+        val date: LocalDate = LocalDate.of(2025, 1, 6)
+
+        When("다음 주로 이동하면") {
+            val weekNumberCalculator = WeekNumberCalculator(date)
+            val initWeekNumber = weekNumberCalculator.currentWeekNumber
+            weekNumberCalculator.moveToNextWeek()
+            val actualCurrentWeekNumber = weekNumberCalculator.currentWeekNumber
+
+            Then("현재 주 번호는 202503이다") {
+                initWeekNumber shouldBe 202502
+                actualCurrentWeekNumber shouldBe 202503
             }
         }
 
-        When("202502에서 -1 주 이동하면") {
-            val shiftedWeek: Int = WeekNumberCalculator.shiftWeekNumber(202502, -1)
+        When("이전 주로 이동하면") {
+            val weekNumberCalculator = WeekNumberCalculator(date)
+            val initWeekNumber = weekNumberCalculator.currentWeekNumber
+            weekNumberCalculator.moveToPreviousWeek()
+            val actualCurrentWeekNumber = weekNumberCalculator.currentWeekNumber
 
-            Then("202501이 되어야 한다") {
-                shiftedWeek shouldBe 202501
+            Then("현재 주 번호는 202501이다") {
+                initWeekNumber shouldBe 202502
+                actualCurrentWeekNumber shouldBe 202501
             }
         }
+    }
 
-        When("202501에서 -1 주 이동하면") {
-            val shiftedWeek: Int = WeekNumberCalculator.shiftWeekNumber(202501, -1)
+    Given("날짜가 2025년 1월 1일로 주 번호가 202501이고") {
+        val date: LocalDate = LocalDate.of(2025, 1, 1)
 
-            Then("2024년의 마지막 주, 즉 202452가 되어야 한다") {
-                shiftedWeek shouldBe 202452
+        When("이전 주로 이동하면") {
+            val weekNumberCalculator = WeekNumberCalculator(date)
+            val initWeekNumber = weekNumberCalculator.currentWeekNumber
+            weekNumberCalculator.moveToPreviousWeek()
+            val actualCurrentWeekNumber = weekNumberCalculator.currentWeekNumber
+
+            Then("현재 주 번호는 202452이다.") {
+                initWeekNumber shouldBe 202501
+                actualCurrentWeekNumber shouldBe 202452
             }
         }
+    }
 
-        When("202452에서 +1 주 이동하면") {
-            val shiftedWeek: Int = WeekNumberCalculator.shiftWeekNumber(202452, 1)
+    Given("날짜가 2024년 12월 29일(일)로 주 번호가 202452이고") {
+        val date: LocalDate = LocalDate.of(2024, 12, 29)
 
-            Then("2025년의 첫 주, 즉 202501이 되어야 한다") {
-                shiftedWeek shouldBe 202501
+        When("다음 주로 이동하면") {
+            val weekNumberCalculator = WeekNumberCalculator(date)
+            val initWeekNumber = weekNumberCalculator.currentWeekNumber
+            weekNumberCalculator.moveToNextWeek()
+            val actualCurrentWeekNumber = weekNumberCalculator.currentWeekNumber
+
+            Then("현재 주 번호는 202501이다.") {
+                initWeekNumber shouldBe 202452
+                actualCurrentWeekNumber shouldBe 202501
             }
         }
     }
