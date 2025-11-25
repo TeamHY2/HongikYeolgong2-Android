e4ae9ebdeb592ea7d8f55613e1d65ae11bc359b8
Bandal <no1msh1217@gmail.com>
2025-11-25 18:58:22 +0900

    feat: 친구 객체에 유저ID, 닉네임, 공부시간 추가

--- friend-domain/src/main/java/com/teamhy2/friend/domain/model/Friend.kt
+++ friend-domain/src/main/java/com/teamhy2/friend/domain/model/Friend.kt
@@ -1,6 +1,10 @@
 package com.teamhy2.friend.domain.model
 
+import kotlin.time.Duration
+
 data class Friend(
     val id: Long,
-    val name: String,
+    val userId: Long,
+    val nickname: String,
+    val studyTime: Duration,
 )
