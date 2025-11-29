8e6323f03b478de7106921a99936fae7221c5668
Bandal <no1msh1217@gmail.com>
2025-11-29 19:20:54 +0900

    feat: 친구 공부시간 조회 ViewModel 및 Contract 구현

--- friend-presentation/src/main/java/com/teamhy2/friend/FriendViewModel.kt
+++ friend-presentation/src/main/java/com/teamhy2/friend/FriendViewModel.kt
@@ -1,6 +1,7 @@
 package com.teamhy2.friend
 
 import com.teamhy2.designsystem.util.mvi.MviViewModel
+import com.teamhy2.friend.domain.model.DateType
 import com.teamhy2.friend.domain.repository.FriendRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -16,14 +17,48 @@ class FriendViewModel
             intent: FriendUiIntent,
         ): FriendUiState {
             return when (intent) {
-                FriendUiIntent.EnterFriendScreen -> fetchFriends(current)
+                FriendUiIntent.EnterFriendScreen -> {
+                    val dateType =
+                        when (current) {
+                            is FriendUiState.Loaded -> current.selectedRecordFilterType.toDateType()
+                            FriendUiState.Loading -> DateType.MONTHLY
+                        }
+                    fetchFriends(current, dateType)
+                }
+                is FriendUiIntent.ChangeRecordFilterType -> {
+                    val newState =
+                        when (current) {
+                            is FriendUiState.Loaded ->
+                                current.copy(selectedRecordFilterType = intent.recordFilterType)
+                            FriendUiState.Loading ->
+                                FriendUiState.Loaded(
+                                    selectedRecordFilterType = intent.recordFilterType,
+                                    isNotificationOn = false,
+                                    friends = emptyList(),
+                                )
+                        }
+                    fetchFriends(newState, intent.recordFilterType.toDateType())
+                }
             }
         }
 
-        private suspend fun fetchFriends(current: FriendUiState): FriendUiState {
-            return friendRepository.fetchFriends()
+        private suspend fun fetchFriends(
+            current: FriendUiState,
+            dateType: DateType,
+        ): FriendUiState {
+            return friendRepository.fetchFriends(dateType)
                 .fold(
-                    onSuccess = { friends -> FriendUiState.Loading },
+                    onSuccess = { friends ->
+                        when (current) {
+                            is FriendUiState.Loaded -> current.copy(friends = friends)
+                            FriendUiState.Loading ->
+                                FriendUiState.Loaded(
+                                    selectedRecordFilterType = dateType.toRecordFilterType(),
+                                    isNotificationOn = false,
+                                    friends = friends,
+                                )
+                        }
+                    },
                     onFailure = {
                         postSideEffect(FriendSideEffect.ShowSnackBar(it.message ?: "알 수 없는 오류가 발생했습니다."))
                         current
