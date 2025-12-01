7c6af14453d22ab001a5f615f9d78186a816da75
Bandal <no1msh1217@gmail.com>
2025-12-01 20:58:36 +0900

    style: 뷰모델 @Inject constructor 한 줄 정렬

--- main-presentation/src/main/java/com/teamhy2/feature/home/HomeViewModel.kt
+++ main-presentation/src/main/java/com/teamhy2/feature/home/HomeViewModel.kt
@@ -26,126 +26,124 @@ import kotlin.coroutines.suspendCoroutine
 
 @OptIn(OrbitExperimental::class)
 @HiltViewModel
-class HomeViewModel
-    @Inject
-    constructor(
-        private val wiseSayingRepository: WiseSayingRepository,
-        private val studyRepository: StudyRepository,
-        private val promotionRepository: PromotionRepository,
-        private val userRepository: UserRepository,
-    ) : ViewModel(), ContainerHost<HomeState, HomeSideEffect> {
-        override val container: Container<HomeState, HomeSideEffect> =
-            container(HomeState.Loading) {
-                loadHomeData()
-                loadPromotionData()
-                updateDeviceToken()
-            }
+class HomeViewModel @Inject constructor(
+    private val wiseSayingRepository: WiseSayingRepository,
+    private val studyRepository: StudyRepository,
+    private val promotionRepository: PromotionRepository,
+    private val userRepository: UserRepository,
+) : ViewModel(), ContainerHost<HomeState, HomeSideEffect> {
+    override val container: Container<HomeState, HomeSideEffect> =
+        container(HomeState.Loading) {
+            loadHomeData()
+            loadPromotionData()
+            updateDeviceToken()
+        }
 
-        private suspend fun loadHomeData() =
-            subIntent {
-                runCatching {
-                    coroutineScope {
-                        val deferredWiseSaying =
-                            async { wiseSayingRepository.fetchWiseSaying().getOrThrow() }
-                        val deferredWeeklyStudyDays =
-                            async { studyRepository.fetchWeeklyStudyDay().getOrThrow() }
+    private suspend fun loadHomeData() =
+        subIntent {
+            runCatching {
+                coroutineScope {
+                    val deferredWiseSaying =
+                        async { wiseSayingRepository.fetchWiseSaying().getOrThrow() }
+                    val deferredWeeklyStudyDays =
+                        async { studyRepository.fetchWeeklyStudyDay().getOrThrow() }
 
-                        deferredWiseSaying.await() to deferredWeeklyStudyDays.await()
-                    }
+                    deferredWiseSaying.await() to deferredWeeklyStudyDays.await()
                 }
-                    .onSuccess { (wiseSaying, weeklyStudyDays) ->
-                        reduce {
-                            HomeState.Success(
-                                wiseSaying = wiseSaying,
-                                weeklyStudyDays = weeklyStudyDays.toImmutableList(),
-                            )
-                        }
-                    }
-                    .onFailure { postSideEffect(HomeSideEffect.ShowError(it)) }
             }
-
-        private suspend fun loadPromotionData() =
-            subIntent {
-                runCatching {
-                    val promotion: Promotion = promotionRepository.fetchPromotionData().getOrThrow()
-                    val isDismissedFlow: Flow<Boolean> = promotionRepository.isPromotionDismissed
-
-                    isDismissedFlow.collect { isDismissed ->
-                        runOn<HomeState.Success> {
-                            reduce {
-                                state.copy(
-                                    promotion = promotion,
-                                    isPromotionDialog = isDismissed.not() && promotion.isActive,
-                                )
-                            }
-                        }
-                    }
-                }.onFailure { postSideEffect(HomeSideEffect.ShowError(it)) }
-            }
-
-        fun increaseTodayStudyCount() =
-            intent {
-                runOn<HomeState.Success> {
+                .onSuccess { (wiseSaying, weeklyStudyDays) ->
                     reduce {
-                        val updatedWeeklyStudyDays: List<WeeklyStudyDay> =
-                            state.weeklyStudyDays.map { studyDay ->
-                                if (studyDay.date ==
-                                    LocalDate.now()
-                                        .format(DateTimeFormatter.ofPattern("M/dd"))
-                                ) {
-                                    studyDay.copy(studyCount = studyDay.studyCount + 1)
-                                } else {
-                                    studyDay
-                                }
-                            }
-                        state.copy(weeklyStudyDays = updatedWeeklyStudyDays.toImmutableList())
+                        HomeState.Success(
+                            wiseSaying = wiseSaying,
+                            weeklyStudyDays = weeklyStudyDays.toImmutableList(),
+                        )
                     }
                 }
-            }
-
-        fun updatePromotionDismissPeriod(
-            startDate: LocalDate,
-            endDate: LocalDate,
-        ) = intent {
-            promotionRepository.savePromotionDismissPeriod(startDate, endDate)
                 .onFailure { postSideEffect(HomeSideEffect.ShowError(it)) }
         }
 
-        fun updatePromotionDialogVisibility(isVisible: Boolean) =
-            intent {
-                runOn<HomeState.Success> {
-                    reduce { state.copy(isPromotionDialog = isVisible) }
+    private suspend fun loadPromotionData() =
+        subIntent {
+            runCatching {
+                val promotion: Promotion = promotionRepository.fetchPromotionData().getOrThrow()
+                val isDismissedFlow: Flow<Boolean> = promotionRepository.isPromotionDismissed
+
+                isDismissedFlow.collect { isDismissed ->
+                    runOn<HomeState.Success> {
+                        reduce {
+                            state.copy(
+                                promotion = promotion,
+                                isPromotionDialog = isDismissed.not() && promotion.isActive,
+                            )
+                        }
+                    }
+                }
+            }.onFailure { postSideEffect(HomeSideEffect.ShowError(it)) }
+        }
+
+    fun increaseTodayStudyCount() =
+        intent {
+            runOn<HomeState.Success> {
+                reduce {
+                    val updatedWeeklyStudyDays: List<WeeklyStudyDay> =
+                        state.weeklyStudyDays.map { studyDay ->
+                            if (studyDay.date ==
+                                LocalDate.now()
+                                    .format(DateTimeFormatter.ofPattern("M/dd"))
+                            ) {
+                                studyDay.copy(studyCount = studyDay.studyCount + 1)
+                            } else {
+                                studyDay
+                            }
+                        }
+                    state.copy(weeklyStudyDays = updatedWeeklyStudyDays.toImmutableList())
                 }
             }
-
-        private suspend fun updateDeviceToken() =
-            subIntent {
-                val token =
-                    suspendCoroutine { continuation ->
-                        FirebaseMessaging
-                            .getInstance()
-                            .token
-                            .addOnCompleteListener { task ->
-                                if (!task.isSuccessful) {
-                                    continuation.resume(null)
-                                } else {
-                                    continuation.resume(task.result)
-                                }
-                            }
-                    }
-
-                if (token == null) return@subIntent
-
-                userRepository.updateDeviceToken(token)
-                    .onSuccess {
-                        Log.d(TAG, "Device token updated successfully")
-                    }
-                    .onFailure { exception ->
-                        Log.e(TAG, "Failed to update device token", exception)
-                    }
-            }
-
-        companion object {
-            private const val TAG = "HomeViewModel"
         }
+
+    fun updatePromotionDismissPeriod(
+        startDate: LocalDate,
+        endDate: LocalDate,
+    ) = intent {
+        promotionRepository.savePromotionDismissPeriod(startDate, endDate)
+            .onFailure { postSideEffect(HomeSideEffect.ShowError(it)) }
     }
+
+    fun updatePromotionDialogVisibility(isVisible: Boolean) =
+        intent {
+            runOn<HomeState.Success> {
+                reduce { state.copy(isPromotionDialog = isVisible) }
+            }
+        }
+
+    private suspend fun updateDeviceToken() =
+        subIntent {
+            val token =
+                suspendCoroutine { continuation ->
+                    FirebaseMessaging
+                        .getInstance()
+                        .token
+                        .addOnCompleteListener { task ->
+                            if (!task.isSuccessful) {
+                                continuation.resume(null)
+                            } else {
+                                continuation.resume(task.result)
+                            }
+                        }
+                }
+
+            if (token == null) return@subIntent
+
+            userRepository.updateDeviceToken(token)
+                .onSuccess {
+                    Log.d(TAG, "Device token updated successfully")
+                }
+                .onFailure { exception ->
+                    Log.e(TAG, "Failed to update device token", exception)
+                }
+        }
+
+    companion object {
+        private const val TAG = "HomeViewModel"
+    }
+}
