181ecd8e7838cbb6782bebbb8a44e1d2bf0bcfdc
librarywon <librarywon@naver.com>
2025-12-06 00:23:23 +0900

    refactor: immutableList 적용

--- friend-presentation/src/main/java/com/teamhy2/friend/notification/FriendNotificationContract.kt
+++ friend-presentation/src/main/java/com/teamhy2/friend/notification/FriendNotificationContract.kt
@@ -1,10 +1,12 @@
 package com.teamhy2.friend.notification
 
 import com.teamhy2.friend.domain.model.FriendNotification
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 data class FriendNotificationUiState(
     val isLoading: Boolean = false,
-    val notifications: List<FriendNotification> = emptyList(),
+    val notifications: ImmutableList<FriendNotification> = persistentListOf(),
 )
 
 sealed interface FriendNotificationEffect {
