f0eb6ad200f62c99ea0a8ed8141a708849473c99
Bandal <no1msh1217@gmail.com>
2026-06-21 13:04:16 +0900

    refactor: 사용하지 않는 GetCredentialException import 제거

--- core/auth/src/main/java/com/teamhy2/core/auth/GoogleSignIn.kt
+++ core/auth/src/main/java/com/teamhy2/core/auth/GoogleSignIn.kt
@@ -7,7 +7,6 @@ import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
-import androidx.credentials.exceptions.GetCredentialException
 import androidx.credentials.exceptions.NoCredentialException
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
