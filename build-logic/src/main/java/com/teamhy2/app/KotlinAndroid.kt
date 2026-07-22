53f0b010ccb5fb29094d01243889c820974c0a61
librarywon <librarywon@naver.com>
2026-07-22 12:05:19 +0900

    ⬆️ targetSdk 36으로 상향 (compileSdk 36)

--- build-logic/src/main/java/com/teamhy2/app/KotlinAndroid.kt
+++ build-logic/src/main/java/com/teamhy2/app/KotlinAndroid.kt
@@ -11,7 +11,7 @@ internal fun Project.configureKotlinAndroid() {
 
 	// Android
 	androidExtension.apply {
-		compileSdk = 35
+		compileSdk = 36
 		defaultConfig {
 			minSdk = 30
 			testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
